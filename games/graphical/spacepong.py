6d4d25f2a5e4bb1af9fa1988b07517b512944c46
Methsaan <methsaan@gmail.com>
2018-03-31 20:27:29 -0400

    updates

--- games/graphical/spacepong.py
+++ games/graphical/spacepong.py
@@ -1,31 +1,69 @@
 #! /usr/bin/python3
 
+# Useful modules
 import random
 import time
 import math
 from tkinter import *
-tk = Tk()
+# Constants
 WIDTH = 1000
 HEIGHT = 1000
+# Useful objects
+tk = Tk()
 tk.title("Tkinter spacepong game")
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
 def spacepong():
+    # black background
     canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill="black")
-    canvas.create_oval(-100, 600, 1100, 1400, fill="orange", outline="darkorange")
+    # sun
+    canvas.create_oval(350, 500, 650, 800, fill="orange", outline="darkorange")
     def star(color, size, minus, plus, a, b, border, col2):
+        # random star
         spot = random.randrange(minus, plus)
         spot2 = random.randrange(minus, plus)
         canvas.create_oval(spot, spot2-a, spot+size, spot2+size-b, fill=color, outline=col2, width=border)
+    # alien
+    canvas.create_oval(100, 100, 200, 200, fill="green2", outline="darkgreen", width=7)
+    canvas.create_oval(100, 200, 200, 400, fill="green2", outline="darkgreen", width=7)
+    canvas.create_line(120, 170, 180, 170, fill="darkgreen", width=7)
+    canvas.create_rectangle(130, 130, 145, 145, fill="darkgreen", width=0)
+    canvas.create_rectangle(170, 130, 185, 145, fill="darkgreen", width=0)
+    canvas.create_line(75, 75, 100, 140, fill="darkgreen", width=7)
+    canvas.create_line(225, 75, 200, 140, fill="darkgreen", width=7)
+    canvas.create_oval(65, 75, 95, 105, fill="green2", outline="darkgreen", width=3)
+    canvas.create_oval(210, 70, 240, 100, fill="green2", outline="darkgreen", width=3)
+    canvas.create_line(50, 250, 120, 250, fill="darkgreen", width=21)
+    canvas.create_line(200, 250, 250, 250, fill="darkgreen", width=21)
+    canvas.create_line(125, 400, 100, 540, fill="darkgreen", width=21)
+    canvas.create_line(175, 400, 200, 540, fill="darkgreen", width=21)
+    for x in range(1600):
+        # creates the star
+        star('white', random.randrange(1, 17), -10, 1600, 280, 280, 3, 'gray33')
     for x in range(300):
-        star('white', 15, -10, 940, 240, 240, 6, 'gray33')
-    for x in range(800):
-        spotx = random.randrange(-100, 1100)
-        spoty = random.randrange(560, 1000)
-        canvas.create_rectangle(spotx, spoty, spotx+random.randrange(-30, 30), spoty+random.randrange(-30, 30), fill="orangered", outline="orangered", width=2)
+        # sunshine
+        spotx = random.randrange(350, 650)
+        spoty = random.randrange(500, 800)
+        canvas.create_rectangle(spotx, spoty, spotx+random.randrange(-20, 20), spoty+random.randrange(-20, 20), fill="orangered", outline="orangered", width=2)
+    # mercury
+    canvas.create_oval(100, 800, 150, 850, fill="rosybrown4")
+    # venus
+    canvas.create_oval(800, 825, 900, 925, fill="orangered")
+    # astronaut
+    canvas.create_rectangle(625, 75, 725, 175, fill="gray90", width=4, outline="gray70")
+    canvas.create_rectangle(645, 95, 705, 155, fill="tomato", outline="tomato")
+    canvas.create_rectangle(650, 175, 750, 325, fill="gray90", width=4, outline="gray70")
+    canvas.create_line(650, 175, 600, 275, fill="gray60", width=28)
+    canvas.create_line(750, 175, 700, 275, fill="gray60", width=28)
+    canvas.create_rectangle(650, 325, 700, 425, fill="gray60", outline="black", width=5)
+    canvas.create_rectangle(700, 325, 750, 425, fill="gray60", outline="black", width=5)
+    canvas.create_line(675, 400, 700, 550, fill="gray50", width=45)
+    canvas.create_line(725, 400, 750, 550, fill="gray50", width=45)
+    # ball speed
     x = random.randrange(-12, 12)
     y = random.randrange(11, 17)
+    # define the direction of the meteor
     if x == 0:
         x = 6
     if x >= -12 and x <= -9:
@@ -40,28 +78,27 @@ def spacepong():
         n = 570
     elif x >= 9 and x <= 12:
         n = 590
-    track = canvas.create_rectangle(0, 935, 1000, 965, fill="orange", outline="orange")
+    # paddle and meteor objects
+    #track = canvas.create_rectangle(0, 935, 1000, 965, fill="orange", outline="orange")
     paddle = canvas.create_rectangle(300, HEIGHT-65, 600, HEIGHT-35, fill='blue2', outline='blue2')
     earthtext = canvas.create_text(470, 950, fill="blue4", text="EARTH", font=('helvetica', 25))
     fire = canvas.create_polygon(467, 500, 533, 500, n, 340, fill="orange", outline="darkorange")
     ball = canvas.create_oval((WIDTH/2)-30, (HEIGHT/2)-30, (WIDTH/2)+30, (HEIGHT/2)+30, fill="lightgoldenrod2", outline="peachpuff")
-    canvas.create_oval(100, 100, 200, 200, fill="green2", outline="white", width=7)
-    canvas.create_oval(100, 200, 200, 400, fill="green2", outline="white", width=7)
-    canvas.create_line(120, 170, 180, 170, fill="white", width=7)
-    canvas.create_rectangle(130, 130, 135, 135, fill="white", width=0)
-    canvas.create_rectangle(170, 130, 175, 135, fill="white", width=0)
     while True:
+        # move the ball
         canvas.move(ball, x, y)
         canvas.move(fire, x, y)
         pos = canvas.coords(ball)
         pos2 = canvas.coords(fire)
         pos3 = canvas.coords(paddle)
+        # detect whether the ball hit the wall
         if pos[3] >= WIDTH-65 or pos2[1] < 0:
             y = -y
         if pos[2] >= HEIGHT or pos[0] < 0:
             x = -x
         tk.update()
         time.sleep(0.01)
+        # moves the paddle depending on the user's key symbol
         def move_paddle(event):
             if event.keysym == 'Left':
                 canvas.move(paddle, -35, 0)
@@ -71,5 +108,6 @@ def spacepong():
                 canvas.move(earthtext, 35, 0)
         canvas.bind_all('<KeyPress-Left>', move_paddle)
         canvas.bind_all('<KeyPress-Right>', move_paddle)
+# calls the spacepong() function
 spacepong()
 canvas.mainloop()
