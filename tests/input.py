d963c2572002cc8633bd0e8ee588d98e1000aae0
Methsaan Wick <methsaan@gmail.com>
2018-05-15 16:07:17 -0400

    update

---
+++ tests/input.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+if age > 25:
+	print("You are old " + name)
+else:
+	print("You are young " + name)
