6febf30651bf66d346d7926072730fbd654ad922
Methsaan <methsaan@gmail.com>
2018-05-21 16:53:50 -0400

    update

--- usefulprograms/fraction.py
+++ usefulprograms/fraction.py
@@ -3,20 +3,20 @@
 import random
 point = 0
 for x in range(2):
-	a = str(round(random.uniform(10.0, 99.0), 2))
-	b = str(round(random.uniform(10.0, 99.0), 2))
+	a = str(random.randrange(100, 999)/100)
+	b = str(random.randrange(100, 999)/100)
 	x = float(input("What is " + a + " + " + b + "? "))
 	if float(a) + float(b) == x:
 		point = point + 1
 for x in range(2):
-	a = str(round(random.uniform(1.0, 9.0), 2))
-	b = str(round(random.uniform(1.0, 9.0), 1))
+	a = str(random.randrange(100, 999)/100)
+	b = str(random.randrange(100, 999)/100)
 	x = float(input("What is " + a + " x " + b + "? "))
 	if float(a) * float(b) == x:
 		point = point + 1
 for x in range(2):
-	a = str(round(random.uniform(10.0, 99.0), 2))
-	b = str(round(random.uniform(10.0, 99.0), 2))
+	a = str(random.randrange(100, 990)/100)
+	b = str(random.randrange(100, 990)/100)
 	x = float(input("What is " + a + " - " + b + "? "))
 	if float(a) - float(b) == x:
 		point = point + 1
--- usefulprograms/quadplotter.py
+++ usefulprograms/quadplotter.py
@@ -5,34 +5,37 @@ import time
 import random
 
 tk = Tk()
+plotx1 = None
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
-canvas.create_line(50, 0, 50, 400)
-canvas.create_line(100, 0, 100, 400)
-canvas.create_line(150, 0, 150, 400)
-canvas.create_line(200, 0, 200, 400)
-canvas.create_line(250, 0, 250, 400)
-canvas.create_line(300, 0, 300, 400)
-canvas.create_line(350, 0, 350, 400)
-canvas.create_line(400, 0, 400, 400)
-canvas.create_line(0, 50, 400, 50)
-canvas.create_line(0, 100, 400, 100)
-canvas.create_line(0, 150, 400, 150)
-canvas.create_line(0, 200, 400, 200)
-canvas.create_line(0, 250, 400, 250)
-canvas.create_line(0, 300, 400, 300)
-canvas.create_line(0, 350, 400, 350)
-canvas.create_line(0, 400, 400, 400)
-plotx1 = int(input("Enter first coordinate x: ")) * 50
-ploty1 = int(input("Enter first coordinate y: ")) * 50
-plotx2 = int(input("Enter second coordinate x: ")) * 50
-ploty2 = int(input("Enter second coordinate y: ")) * 50
-plotx3 = int(input("Enter third coordinate x: ")) * 50
-ploty3 = int(input("Enter third coordinate y: ")) * 50
-plotx4 = int(input("Enter fourth coordinate x: ")) * 50
-ploty4 = int(input("Enter fourth coordinate y: ")) * 50
-print("loading quadrilateral ...      ", end="", flush=True)
-time.sleep(3)
-print("[done]")
-canvas.create_polygon(plotx1, ploty1, plotx2, ploty2, plotx3, ploty3, plotx4, ploty4, fill="gray60", outline="gray60")
+while plotx1 != 1000:
+    canvas.create_line(50, 0, 50, 400, width=2)
+    canvas.create_line(100, 0, 100, 400, width=2)
+    canvas.create_line(150, 0, 150, 400, width=2)
+    canvas.create_line(200, 0, 200, 400, width=2)
+    canvas.create_line(250, 0, 250, 400, width=2)
+    canvas.create_line(300, 0, 300, 400, width=2)
+    canvas.create_line(350, 0, 350, 400, width=2)
+    canvas.create_line(400, 0, 400, 400, width=2)
+    canvas.create_line(0, 50, 400, 50, width=2)
+    canvas.create_line(0, 100, 400, 100, width=2)
+    canvas.create_line(0, 150, 400, 150, width=2)
+    canvas.create_line(0, 200, 400, 200, width=2)
+    canvas.create_line(0, 250, 400, 250, width=2)
+    canvas.create_line(0, 300, 400, 300, width=2)
+    canvas.create_line(0, 350, 400, 350, width=2)
+    canvas.create_line(0, 400, 400, 400, width=2)
+    print("Welcome to the quadrilateral plotter. Type 1000 for the following input to quit")
+    plotx1 = int(input("Enter first coordinate x: ")) * 50
+    ploty1 = int(input("Enter first coordinate y: ")) * 50
+    plotx2 = int(input("Enter second coordinate x: ")) * 50
+    ploty2 = int(input("Enter second coordinate y: ")) * 50
+    plotx3 = int(input("Enter third coordinate x: ")) * 50
+    ploty3 = int(input("Enter third coordinate y: ")) * 50
+    plotx4 = int(input("Enter fourth coordinate x: ")) * 50
+    ploty4 = int(input("Enter fourth coordinate y: ")) * 50
+    print("loading quadrilateral ...      ", end="", flush=True)
+    time.sleep(3)
+    print("[done]")
+    canvas.create_polygon(plotx1, ploty1, plotx2, ploty2, plotx3, ploty3, plotx4, ploty4, outline="red")
 canvas.mainloop()
