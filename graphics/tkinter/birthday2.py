a6ab3b2e641c3cecea9c5a5e33ccd0e11a9fca91
Methsaan <methsaan@gmail.com>
2018-05-26 16:28:50 -0400

    update

--- graphics/tkinter/birthday2.py
+++ graphics/tkinter/birthday2.py
@@ -4,7 +4,7 @@ import random
 import time
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=600, height=600, bg="black")
+canvas = Canvas(tk, width=600, height=600, bg="lightgreen", bd=0, highlightthickness=0)
 canvas.pack()
 shade = "gray95"
 canvas.create_text(300, 300, fill=shade, text="Happy birthday", font=('helvetica', 40))
@@ -13,20 +13,19 @@ b = 0
 c = 600
 d = 100
 transparency = 95
-for x in range(500):
-    colors1 = random.choice(['blue', 'red', 'yellow', 'orange', 'purple', 'green2', 'aqua'])
-    colors2 = random.choice(['blue', 'red', 'yellow', 'orange', 'purple', 'green2', 'aqua'])
+for x in range(168):
+    colors1 = random.choice(['green', 'green2', 'green3', 'green4', 'springgreen', 'forestgreen', 'lightgreen', 'darkgreen', 'greenyellow', 'yellowgreen'])
+    colors2 = random.choice(['blue', 'aqua', 'lightblue', 'blue2', 'blue3', 'blue4', 'navy'])
     canvas.create_rectangle(a, b, c, d, fill=colors1, outline=colors2)
     tk.update()
-    a = a - 1
-    b = b + 1
-    c = c - 1
-    d = d + 1
-    transparency -= 2
+    a = a - 3
+    b = b + 3
+    c = c - 3
+    d = d + 3
+    transparency -= 6
     if transparency <= 0:
         transparency = 95
     shade = "gray" + str(transparency)
     canvas.create_text(300, 300, fill=shade, text="Happy birthday", font=('helvetica', 40))
-    time.sleep(0.001)
     tk.update()
 canvas.mainloop()
