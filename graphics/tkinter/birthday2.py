c9ef93cdb3fba6ffdae9c87f73c01359078613ae
Methsaan Wick <methsaan@gmail.com>
2018-05-26 11:35:27 -0400

    update

--- graphics/tkinter/birthday2.py
+++ graphics/tkinter/birthday2.py
@@ -3,7 +3,7 @@
 import time
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=600, height=600, bg="white")
+canvas = Canvas(tk, width=600, height=600, bg="orange")
 canvas.pack()
 shade = "gray95"
 canvas.create_text(300, 300, fill=shade, text="Happy birthday", font=('helvetica', 40))
@@ -11,5 +11,19 @@ a = 500
 b = 0
 c = 600
 d = 100
-canvas.create_rectangle(a, b, c, d, fill="blue2", outline="blue4")
+transparency = 95
+for x in range(500):
+    canvas.create_rectangle(a, b, c, d, fill="blue", outline="aqua")
+    tk.update()
+    a = a - 1
+    b = b + 1
+    c = c - 1
+    d = d + 1
+    transparency -= 2
+    if transparency <= 0:
+        transparency = 95
+    shade = "gray" + str(transparency)
+    canvas.create_text(300, 300, fill=shade, text="Happy birthday", font=('helvetica', 40))
+    time.sleep(0.01)
+    tk.update()
 canvas.mainloop()
