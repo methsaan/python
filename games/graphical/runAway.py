0e56e3c199ea239344fa33a27bd7f020a03978cf
Methsaan <methsaan@gmail.com>
2018-06-02 17:09:24 -0400

    update

--- games/graphical/runAway.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-from tkinter import *
-import time
-import random
-
-tk = Tk()
-canvas = Canvas(tk, width=600, height=600, bg="orange")
-monster = canvas.create_rectangle(500, 0, 600, 100, fill="red")
