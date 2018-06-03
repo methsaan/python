e15693e17ae0a195c5f0123a4331b9b509f2e95c
Methsaan <methsaan@gmail.com>
2018-06-03 10:20:52 -0400

    update

--- usefulprograms/quadplotter.py
+++ usefulprograms/quadplotter.py
@@ -3,32 +3,36 @@
 from tkinter import *
 import time
 import random
+import subprocess
 
 tk = Tk()
 plotx1 = None
 canvas = Canvas(tk, width=400, height=400)
 canvas.pack()
+canvas.create_line(50, 0, 50, 400, width=2)
+canvas.create_line(100, 0, 100, 400, width=2)
+canvas.create_line(150, 0, 150, 400, width=2)
+canvas.create_line(200, 0, 200, 400, width=2)
+canvas.create_line(250, 0, 250, 400, width=2)
+canvas.create_line(300, 0, 300, 400, width=2)
+canvas.create_line(350, 0, 350, 400, width=2)
+canvas.create_line(400, 0, 400, 400, width=2)
+canvas.create_line(0, 50, 400, 50, width=2)
+canvas.create_line(0, 100, 400, 100, width=2)
+canvas.create_line(0, 150, 400, 150, width=2)
+canvas.create_line(0, 200, 400, 200, width=2)
+canvas.create_line(0, 250, 400, 250, width=2)
+canvas.create_line(0, 300, 400, 300, width=2)
+canvas.create_line(0, 350, 400, 350, width=2)
+canvas.create_line(0, 400, 400, 400, width=2)
 while True:
-    canvas.create_line(50, 0, 50, 400, width=2)
-    canvas.create_line(100, 0, 100, 400, width=2)
-    canvas.create_line(150, 0, 150, 400, width=2)
-    canvas.create_line(200, 0, 200, 400, width=2)
-    canvas.create_line(250, 0, 250, 400, width=2)
-    canvas.create_line(300, 0, 300, 400, width=2)
-    canvas.create_line(350, 0, 350, 400, width=2)
-    canvas.create_line(400, 0, 400, 400, width=2)
-    canvas.create_line(0, 50, 400, 50, width=2)
-    canvas.create_line(0, 100, 400, 100, width=2)
-    canvas.create_line(0, 150, 400, 150, width=2)
-    canvas.create_line(0, 200, 400, 200, width=2)
-    canvas.create_line(0, 250, 400, 250, width=2)
-    canvas.create_line(0, 300, 400, 300, width=2)
-    canvas.create_line(0, 350, 400, 350, width=2)
-    canvas.create_line(0, 400, 400, 400, width=2)
-    print("Welcome to the quadrilateral plotter. Type 1000 for the following input to quit")
+    print("Welcome to the quadrilateral plotter. Type -1 for the following input to quit")
     plotx1 = int(input("Enter first coordinate x: ")) * 50
-    if plotx1 == 1000:
-        break
+    if plotx1 == -50:
+        print("Quitting ...               ", end="", flush=True)
+        time.sleep(1)
+        print("[DONE]")
+        quit()
     ploty1 = int(input("Enter first coordinate y: ")) * 50
     plotx2 = int(input("Enter second coordinate x: ")) * 50
     ploty2 = int(input("Enter second coordinate y: ")) * 50
@@ -37,7 +41,7 @@ while True:
     plotx4 = int(input("Enter fourth coordinate x: ")) * 50
     ploty4 = int(input("Enter fourth coordinate y: ")) * 50
     print("loading quadrilateral ...      ", end="", flush=True)
-    time.sleep(3)
-    print("[done]")
-    canvas.create_polygon(plotx1, ploty1, plotx2, ploty2, plotx3, ploty3, plotx4, ploty4, outline="red")
+    time.sleep(1)
+    print("[DONE]")
+    canvas.create_polygon(plotx1, ploty1, plotx2, ploty2, plotx3, ploty3, plotx4, ploty4, fill="",outline="red", width=3)
 canvas.mainloop()
