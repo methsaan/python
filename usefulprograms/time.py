bac43222756b9d377a03c9e0d154aba768da1674
Methsaan <methsaan@gmail.com>
2018-07-10 15:15:38 -0400

    update

--- usefulprograms/time.py
+++ usefulprograms/time.py
@@ -8,6 +8,6 @@ canvas = Canvas(tk, width=700, height=700)
 canvas.pack()
 while True:
     canvas.create_rectangle(0, 0, 700, 700, fill="lightblue", outline="lightblue")
-    canvas.create_text(350, 350, text=str(time.asctime()), font=('helvetica', 36))
+    canvas.create_text(350, 350, text=str(time.asctime()), font=('helvetica', 46))
     tk.update()
 canvas.mainloop()
