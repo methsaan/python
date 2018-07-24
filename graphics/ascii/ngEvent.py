a007b9504972313d0cf5252c09c52ea6a4a51abb
Methsaan <methsaan@gmail.com>
2018-07-24 09:03:16 -0400

    update

--- graphics/ascii/ngEvent.py
+++ graphics/ascii/ngEvent.py
@@ -3,17 +3,19 @@
 import subprocess as sp
 from tkinter import *
 tk = Tk()
-canvas = Canvas(tk, width=100, height=100)
+canvas = Canvas(tk, width=0, height=0)
+canvas.pack()
 
 x = 0
-while True:
-    sp.call('clear', shell=True)
-    print((" " * x) + "O")
+for x in range(1000000000000000000000000000000000):
+    x += 0
+    sp.call("clear", shell=True)
+    print(" " * x + "O")
     def moveO(event):
-        if event.keysym == "Up":
-            x++
-        elif event.keysym == "down":
-            x--
-    canvas.bind_all("<KeyPress-Up>")
-    canvas.bind_all("<KeyPress-Down>")
-
+        if event.keysym == "Left":
+            x = x + 1
+        elif event.keysym == "Right":
+            x = x - 1
+    canvas.bind_all("<KeyPress-Left>", moveO)
+    canvas.bind_all("<KeyPress-Right>", moveO)
+    canvas.mainloop()
