c8e1529223f15bf4bdd49448d5d44d21dba6f553
Methsaan <methsaan@gmail.com>
2018-08-16 07:35:13 -0400

    update

--- usefulprograms/timer.py
+++ usefulprograms/timer.py
@@ -4,14 +4,14 @@ import subprocess as sp
 import time
 timer = int(input("How long: "))
 def timeup():
-    print("__________  _______                     _____                    __  ")
-    print("    |          |      |\            /| |             |        | |  \ ")
-    print("    |          |      | \          / | |             |        | |   \ ")
-    print("    |          |      |  \        /  | |             |        | |    \ ")
-    print("    |          |      |   \      /   | |___          |        | |___/")
-    print("    |          |      |    \    /    | |              \      /  |  ")
-    print("    |          |      |     \  /     | |               \    /   |  ")
-    print("    |       ___|___   |      \/      | |_____           \__/    |   ")
+    print("__________  _______                     _____ |       _____                       __  ")
+    print("    |          |      |\            /| |      |      /                |        | |  \ ")
+    print("    |          |      | \          / | |            /                 |        | |   \ ")
+    print("    |          |      |  \        /  | |            |                 |        | |    \ ")
+    print("    |          |      |   \      /   | |___         \                 |        | |___/")
+    print("    |          |      |    \    /    | |             \_____            \      /  |  ")
+    print("    |          |      |     \  /     | |                   \            \    /   |  ")
+    print("    |       ___|___   |      \/      | |_____         _____/             \__/    |   ")
 for x in range(timer):
     for x in range(55):
         print(timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer, "", timer)
@@ -20,5 +20,5 @@ for x in range(timer):
     time.sleep(0.05)
     timer = timer - 1
 timeup()
-time.sleep(0.9)
+time.sleep(0.95)
 sp.call('clear', shell=True)
