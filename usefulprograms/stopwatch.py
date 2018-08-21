c7300f9d18ef7b35c42f22560d9ba0444aabc059
Methsaan <methsaan@gmail.com>
2018-08-21 09:51:47 -0400

    update

--- usefulprograms/stopwatch.py
+++ usefulprograms/stopwatch.py
@@ -9,20 +9,32 @@ canvas.pack()
 secs = 0
 mins = 0
 hrs = 0
-secs2 = "0" + str(secs)
-mins2 = "0" + str(mins)
-hrs2 = "0" + str(hrs)
+def printTime(s, m, h):
+    canvas.create_rectangle(0, 0, 700, 700, fill="springgreen")
+    if h < 10:
+        canvas.create_text(250, 350, text="0" + str(h), font=("helvetica", 40))
+    else:
+        canvas.create_text(250, 350, text=str(h))
+    canvas.create_text(300, 350, text=": ", font=("helvetica", 40))
+    if m < 10:
+        canvas.create_text(350, 350, text="0" + str(m), font=("helvetica", 40))
+    else:
+        canvas.create_text(350, 350, text=str(m))
+    canvas.create_text(400, 350, text=": ", font=("helvetica", 40))
+    if s < 10:
+        canvas.create_text(450, 350, text="0" + str(s), font=("helvetica", 40))
+    else:
+        canvas.create_text(450, 350, text=str(s), font=("helvetica", 40))
+    tk.update()
 while True:
-    timestr = str(" " + str(hrs) + ":" + " " + str(mins) + ":" + " " + str(secs))
-    secs += 1
+    printTime(secs, mins, hrs)
+    time.sleep(1)
+    secs = secs + 1
     if secs == 60:
-        mins += 1
         secs = 0
+        mins = mins + 1
     if mins == 60:
-        hrs += 1
         mins = 0
-    time.sleep(0.9)
-    canvas.create_rectangle(0, 0, 700, 700, fill="springgreen", outline="aqua")
-    canvas.create_text(350, 350, text=timestr, font=('helvetica', 45))
+        hrs = hrs + 1
     tk.update()
 canvas.mainloop()
