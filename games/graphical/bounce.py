0f214aea42ea0c8257d19613ad6612ab05e431c7
Methsaan <methsaan@gmail.com>
2018-10-12 18:27:40 -0400

    update

--- games/graphical/bounce.py
+++ games/graphical/bounce.py
@@ -5,12 +5,12 @@ import time
 import math
 from tkinter import *
 tk = Tk()
-WIDTH = 750
-HEIGHT = 750
+WIDTH = 450
+HEIGHT = 450
 tk.title("Tkinter bounce game")
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
 canvas.pack()
-colors = [random.choice(["springgreen", "goldenrod4", "red"]), random.choice(["blue2", "aqua", "brown"]), random.choice(["tomato", "orangered2", "black"])]
+colors = [random.choice(["blue", "black", "orangered"]), random.choice(["springgreen", "gold", "tomato"]), random.choice(["white", "peachpuff", "navajowhite"])]
 canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill=colors[0], outline=colors[0])
 paddle = canvas.create_rectangle(400, HEIGHT-60, 500, HEIGHT-40, fill=colors[1], outline=colors[1])
 ball = canvas.create_oval((WIDTH/2)-15, (HEIGHT/2)-15, (WIDTH/2)+15, (WIDTH/2)+15, fill=colors[2], outline=colors[2])
@@ -23,6 +23,10 @@ if y > -15 and y < 15:
 while True:
     canvas.move(ball, x, y)
     pos = canvas.coords(ball)
+    cnt = 0
+    #for x in pos:
+     #   print(str(cnt) + ": " + str(x))
+      #  cnt = cnt + 1
     if pos[3] >= WIDTH-65 or pos[1] < 0:
         y = -y
         pos = canvas.coords(ball)
@@ -38,11 +42,11 @@ while True:
         pos = canvas.coords(ball)
         if pos[2] > HEIGHT or pos[0] < 0:
             if x > 0:
-                x = random.randrange(10, 30)
+               x = random.randrange(10, 30)
             elif x < 0:
-                x = random.randrange(-30, -10)
+               x = random.randrange(-30, -10)
             else:
-                print("", end="", flush=True)
+               print("", end="", flush=True)
     tk.update()
     time.sleep(0.01)
     def move_paddle(event):
