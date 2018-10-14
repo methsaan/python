57436e08201b0c4fbb8e28faafebd5a774ae7159
Methsaan <methsaan@gmail.com>
2018-10-14 12:32:48 -0400

    update

--- games/nongraphical/inputtimer.py
+++ games/nongraphical/inputtimer.py
@@ -1,12 +1,17 @@
 #! /usr/bin/python3
 
+import random
 import time
 
-time1 = time.time()
-print("Type something")
-x = input()
-while x[len(x)-1] != "~":
-    x = input()
-time2 = time.time()
-timestr = str(time2-time1)
-print("You took " + timestr[0:timestr.index(".")+3] + " seconds to type")
+time_max = 40.00
+tot_time = 0.0
+print("You have 40 seconds")
+while tot_time < time_max:
+    time1 = time.time()
+    x = input("Type something: ")
+    time2 = time.time()
+    timestr = str(time2-time1)
+    print("You took " + timestr[0:timestr.index(".")+3] + " seconds to type")
+    tot_time = tot_time + float(timestr)
+    print("Time left: " + str(time_max-tot_time))
+print("TIME'S UP")
