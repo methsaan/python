a818b3b61c598d9b2a3d4bf5ac92212558ced1fb
Methsaan <methsaan@gmail.com>
2018-10-15 07:10:51 -0400

    drawing circles for worm.py

--- graphics/tkinter/worm.py
+++ graphics/tkinter/worm.py
@@ -4,5 +4,7 @@ from tkinter import *
 tk = Tk()
 canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
-
+a = 0
+for x in range(100):
+    canvas.create_oval(a-50, a-50, a+50, a+50, fill="gray"+str(x))
 canvas.mainloop()
