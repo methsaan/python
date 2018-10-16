2d3274faa8fe664a21e0191d286d2d0358ebc8b4
Methsaan <methsaan@gmail.com>
2018-10-15 20:35:23 -0400

    update

--- graphics/tkinter/worm.py
+++ graphics/tkinter/worm.py
@@ -4,7 +4,13 @@ from tkinter import *
 tk = Tk()
 canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
-a = 0
-for x in range(100):
-    canvas.create_oval(a-50, a-50, a+50, a+50, fill="gray"+str(x))
+a = -200
+b = -200
+for y in range(100):
+    c = 0
+    for x in range(100):
+        canvas.create_oval(a, b, a+500, b+500, fill="gray"+str(c), outline="gray"+str(c))
+        a = a + 0.7
+        b = b + 0.7
+        c = c + 1
 canvas.mainloop()
