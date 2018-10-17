76e438f9326d85902d7c7fb6b3c93663644f1f83
Methsaan <methsaan@gmail.com>
2018-10-17 06:56:58 -0400

    update

--- usefulprograms/findsquareroot.py
+++ usefulprograms/findsquareroot.py
@@ -7,19 +7,17 @@ square = input("Enter number: ")
 
 one_digit_squares = {"1" : 1, "4" : 2, "9" : 3, "16" : 4, "25" : 5, "36" : 6, "49" : 7, "64" : 8, "81" : 9}
 
-last_digit = square[2]
-first_digit = square[0]
+last_digit = square[2:3]
+first_digit = square[0:1]
 
-found_numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-for x in one_digit_squares:
-    if int(x) > 9:
-        if str(int(x)%10) == last_digit:
-            found_numbers[one_digit_squares[str(int(x)%10)]-1] = one_digit_squares[str(x)]
-    else:
-        if x == last_digit:
-            found_numbers[one_digit_squares[str(int(x)%10)]-1] = one_digit_squares[str(x)]
-print(found_numbers)
+cnt = 1
+cnt2 = 1
+cnt3 = cnt
+while cnt < 100:
+    cnt2 = cnt2 + 2
+    cnt += cnt2
+    cnt3 = cnt
+    cnt3 %= 10
 # prove theory of finding squareroot for 3 digit number in range
 # above
 
