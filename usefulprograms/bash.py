5ced6fc358f999250f30deccc21d6a2ce6ba4166
Methsaan <methsaan@gmail.com>
2018-10-23 05:49:43 -0400

    adding history to bash.py

--- usefulprograms/bash.py
+++ usefulprograms/bash.py
@@ -6,13 +6,15 @@ history = []
 while True:
     x = input("Enter a bash command: ")
     history.append(x)
-    if x[0:7] == "python-":
+    if x == "bash.py-history":
+        print(history)
+    elif x[0:7] == "python-":
         inputs = list(map(str, x.split()))
         inputstr = ''
         for c in inputs:
             inputstr += c + " "
         inputstr = inputstr[7:]
         inputstr = inputstr[:-1]
-        exec(str(inputstr))
+        exec(inputstr)
     else:
         sp.call(x, shell=True)
