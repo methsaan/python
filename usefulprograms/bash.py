56a74182237e1e44868ed40803ef8c988aaa5473
Methsaan <methsaan@gmail.com>
2018-10-28 12:33:25 -0400

    adding eval and map for history

--- usefulprograms/bash.py
+++ usefulprograms/bash.py
@@ -1,21 +1,34 @@
 #! /usr/bin/python3
 
 import subprocess as sp
+import tkinter
 
 history = []
+historymap = {}
 while True:
     x = input("Enter a bash command: ")
     history.append(x)
     if x == "bash.py-history":
         for x in range(len(history)):
-            print(x, history[x])
-    elif x[0:7] == "python-":
+            print(x, "", history[x])
+            historymap = dict(str(x), history[x])
+    elif x[0:21] == "bash.py-clear_history":
+        print(historymap["12"])
+    elif x[0:12] == "python-exec-":
         inputs = list(map(str, x.split()))
         inputstr = ''
         for c in inputs:
             inputstr += c + " "
-        inputstr = inputstr[7:]
+        inputstr = inputstr[12:]
         inputstr = inputstr[:-1]
         exec(inputstr)
+    elif x[0:12] == "python-eval-":
+        inputs = list(map(str, x.split()))
+        inputstr = ''
+        for c in inputs:
+            inputstr += c + " "
+        inputstr = inputstr[12:]
+        inputstr = inputstr[:-1]
+        print(eval(inputstr))
     else:
         sp.call(x, shell=True)
