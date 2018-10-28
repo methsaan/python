78e6c5580c52caf4d3dfa3f41cf24dcb6f7b7102
Methsaan <methsaan@gmail.com>
2018-10-28 13:00:16 -0400

    storing the history in a dictionary;

--- usefulprograms/bash.py
+++ usefulprograms/bash.py
@@ -1,19 +1,24 @@
 #! /usr/bin/python3
 
 import subprocess as sp
-import tkinter
+from itertools import izip
 
 history = []
+history2 = []
 historymap = {}
+cnt = 0
 while True:
     x = input("Enter a bash command: ")
+    cnt = cnt + 1
+    history2.append(x)
+    history.append(cnt)
     history.append(x)
     if x == "bash.py-history":
-        for x in range(len(history)):
-            print(x, "", history[x])
-            historymap = dict(str(x), history[x])
+        for x in range(len(history2)):
+            print(x, "", history2[x])
     elif x[0:21] == "bash.py-clear_history":
-        print(historymap["12"])
+        historymap = dict(zip(history[::2], a[1::2]))
+        print("Enter commands to remove from history: ", end="", flush=True)
     elif x[0:12] == "python-exec-":
         inputs = list(map(str, x.split()))
         inputstr = ''
