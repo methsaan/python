d096333d9af77183f8e863feee756c2219d38856
Methsaan <methsaan@gmail.com>
2018-11-04 20:41:02 -0500

    changing speed

--- graphics/tkinter/randcanv.py
+++ graphics/tkinter/randcanv.py
@@ -7,15 +7,14 @@ tk = Tk()
 
 c = []
 for i in range(1, 10):
-    a = Canvas(tk, width=random.randrange(50, 400), height=random.randrange(50, 400), bg="white")
+    a = Canvas(tk, width=random.randrange(50, 400), height=random.randrange(50, 400), bg="black")
     c.append(a)
     a.pack()
 for j in c:
-    f = j.create_rectangle(0, 0, 20, 20, fill="black")
+    f = j.create_rectangle(0, 0, 20, 20, fill="white")
     for q in range(8):
-        time.sleep(0.04)
+        time.sleep(0.02)
         j.move(f, 12, 12)
         tk.update()
-    j.bind_all("<KeyPress-Up>", input)
 for k in c:
     k.mainloop()
