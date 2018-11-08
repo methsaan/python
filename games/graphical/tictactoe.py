e2f4ff968a1b57fecfd907327915e0d8187daa03
Krishna B <42852227+KrishnaB7@users.noreply.github.com>
2018-11-07 17:07:52 -0800

    Review this code.
    I can help you with your project if needed.

--- games/graphical/tictactoe.py
+++ games/graphical/tictactoe.py
@@ -12,87 +12,11 @@ canvas.create_line(0, 100, 300, 100, width=5)
 canvas.create_line(0, 200, 300, 200, width=5)
 verti = [50, 150, 250]
 horiz = [50, 150, 250]
-def xo(event):
-	if event.char == "1":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="1", font=('helvetica', 50))	
-	if event.char == "2":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="2", font=('helvetica', 50))
-	if event.char == "3":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="3", font=('helvetica', 50))
-	if event.char == "4":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="4", font=('helvetica', 50))
-	if event.char == "5":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="5", font=('helvetica', 50))
-	if event.char == "6":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="6", font=('helvetica', 50))
-	if event.char == "7":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="7", font=('helvetica', 50))
-	if event.char == "8":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="8", font=('helvetica', 50))
-	if event.char == "9":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="9", font=('helvetica', 50))
-	if event.char == "a":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="a", font=('helvetica', 50))
-	if event.char == "b":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="b", font=('helvetica', 50))
-	if event.char == "c":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="c", font=('helvetica', 50))
-	if event.char == "d":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="d", font=('helvetica', 50))
-	if event.char == "e":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="e", font=('helvetica', 50))
-	if event.char == "f":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="f", font=('helvetica', 50))
-	if event.char == "g":
-		x = random.choice(horiz)
-		y = random.choice(verti)
-		canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
-		canvas.create_text(x, y, text="g", font=('helvetica', 50))
+def xo(event.char):
+	x = random.choice(horiz)
+	y = random.choice(verti)
+	canvas.create_rectangle(x-50, y-50, x+50, y+50, fill="gray75")
+	canvas.create_text(x, y, text=event.char, font=('helvetica', 50))
 canvas.bind_all('<KeyPress-1>', xo)
 canvas.bind_all('<KeyPress-2>', xo)
 canvas.bind_all('<KeyPress-3>', xo)
