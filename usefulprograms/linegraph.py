a185beb4587363de9f802ff4ad8ea518e76c3f2d
Methsaan <methsaan@gmail.com>
2018-11-18 12:03:28 -0500

    asking for the labels

---
+++ usefulprograms/linegraph.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+from tkinter import *
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500, bg="gray80")
+canvas.pack()
+
+xLabel = input("Enter a label for the x axis: ")
+yLabel = input("Enter a label for the y axis: ")
+canvas.mainloop()
