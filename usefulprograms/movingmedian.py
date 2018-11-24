a8c5b1cefb1c4ba90ffd988900dc716e58e722f8
Methsaan <methsaan@gmail.com>
2018-11-24 08:35:57 -0500

    sorting array

--- usefulprograms/movingmedian.py
+++ usefulprograms/movingmedian.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 median = None
-data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+data = []
 nextnum = None
 
 index = 0
@@ -9,17 +9,22 @@ while nextnum != "QUIT" and index < 40:
     nextnum = input("Enter the next item: ")
     if nextnum != "QUIT":
         nextnum = int(nextnum)
-        data[index] = nextnum
+        data.append(nextnum)
+        if len(data) > 1:
+            for x in range(len(data)-1):
+                tempElement = data[x]
+                for y in range(len(data)-1):
+                    if tempElement > data[y+1]:
+                        temp = tempElement
+                        data[x] = data[x+1]
+                        data[x+1] = temp
         print(data)
-        for x in range(len(data)):
-            if data[x] < data[x]:
-                temp = data[x]
-                data[x] = data[x]
-                data[x] = temp
         if len(data)%2 == 1:
             median = data[int(len(data)/2)]
         else:
             median = (data[int(len(data)/2)-1] + data[int(len(data)/2)]) / 2
         print("The median is now", median)
         index += 1
+    else:
+        quit()
 print("Maximum items")
