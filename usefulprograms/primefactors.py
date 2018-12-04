d8625eddf82d238c1cd84ab22dc1f473efeb709d
Methsaan <methsaan@gmail.com>
2018-12-04 06:50:01 -0500

    printing only prime factors

--- usefulprograms/primefactors.py
+++ usefulprograms/primefactors.py
@@ -1,11 +1,24 @@
 #! /usr/bin/python3
 
+def isPrime(number):
+    factors = []
+    x = 1
+    while True:
+        if float(number/x) == float(int(number/x)):
+            factors.append(x)
+        if x > number:
+            break
+        x = x + 1
+    if len(factors) <= 2:
+        return True
+    else:
+        return False
 number = int(input("Enter a number: "))
-factors = []
 i = 1
 while True:
-    if float(number/i) == float(int(number/i)):
-        print(str(i) + " is a factor of " + str(number))
+    if float(number/i) == float(int(number/i)) and isPrime(i):
+        print(i, "", end="", flush=True)
     if i > number:
         break
     i = i + 1
+print()
