56add8ed9d5f139a4c3f49936d3c69fbd46643cd
Methsaan <methsaan@gmail.com>
2018-12-08 07:06:49 -0500

    printing middle number

--- usefulprograms/movingmedian.py
+++ usefulprograms/movingmedian.py
@@ -2,28 +2,39 @@
 
 median = None
 data = []
+datastr = ""
 nextnum = None
-
 index = 0
+def sort(l):
+    l.append(12345)
+    for x in range(len(l)-1):
+        for y in range(1, len(l)):
+            if l[x] < l[y]:
+                temp = l[x]
+                l[x] = l[y]
+                l[y] = temp
+    del l[-1]
 while nextnum != "QUIT" and index < 40:
-    nextnum = input("Enter the next item: ")
+    if index > 1:
+        nextnum = input("Enter the next item: ")
+    else:
+        nextnum = 2147483648
     if nextnum != "QUIT":
         nextnum = int(nextnum)
         data.append(nextnum)
         if len(data) > 1:
-            for x in range(len(data)-1):
-                tempElement = data[x]
-                for y in range(len(data)-1):
-                    if tempElement > data[y+1]:
-                        temp = tempElement
-                        data[x] = data[x+1]
-                        data[x+1] = temp
-        print(data)
+            sort(data)
+        datastr = data[:-1]
+        datastr = datastr[1:]
+        datastr = str(datastr)
+        if index > 1:
+            print(datastr)
         if len(data)%2 == 1:
             median = data[int(len(data)/2)]
         else:
             median = (data[int(len(data)/2)-1] + data[int(len(data)/2)]) / 2
-        print("The median is now", median)
+        if index > 1:
+            print("The median is now", median)
         index += 1
     else:
         quit()
