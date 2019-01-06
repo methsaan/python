687ccdf271b3a38b7f9347e0a0950f2ed51c3a41
Methsaan <methsaan@gmail.com>
2019-01-06 08:03:50 -0500

    two sides

---
+++ usefulprograms/rightTriangle.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+import turtle
+import math
+
+t = turtle.Pen()
+
+t.color("lightblue")
+t.width(20)
+t.forward(int(input("Enter length of side a: ")))
+t.forward(int(input("Enter length of side b: ")))
