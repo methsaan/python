52a34cca17752275fb1e0f4f758b83e16820ecef
Methsaan Wick@kali <methsaan@gmail.com>
2019-01-07 05:59:57 -0500

    completing triangle

--- usefulprograms/rightTriangle.py
+++ usefulprograms/rightTriangle.py
@@ -2,10 +2,13 @@
 
 import turtle
 import math
-
+import time
 t = turtle.Pen()
-
+t.speed(1)
 t.color("lightblue")
 t.width(20)
 t.forward(int(input("Enter length of side a: ")))
+t.left(90)
 t.forward(int(input("Enter length of side b: ")))
+t.goto(0, 0)
+time.sleep(5)
