95dfa3c9c11c8d33086b91d8e3e550e3a075eead
Methsaan Wick@kali <methsaan@gmail.com>
2019-01-13 12:42:25 -0500

    prompting at the bottom

--- methuki/console/games/apple_pick/green_apple_tree.py
+++ methuki/console/games/apple_pick/green_apple_tree.py
@@ -3,7 +3,6 @@
 import subprocess
 import time
 import random
-
 import sys
 from termcolor import colored, cprint
 
@@ -19,34 +18,38 @@ while True:
  (oUUUO UU UU UUU)    (U UUUUUU O uuUO)     (uUUU uuuUUo OO UUuuu U) 
   (UUuO UU UUUUU)      (uuUUUU UUU uU)       ( UOoU UuuOoUU O O Ou)  
    (UU uO Oo uU)        (UuO uU O UU)         (uUU O UUU O uUUUou)
-      VVVVV               VVVVvvV                  VVvvVV            ''', 'yellow');cprint('''       |v|                 |v||                     ||||              
+      VVVVV               VVVVvvV                  VVvvVV            ''', 'green');cprint('''       |v|                 |v||                     ||||              
        |||                 ||v|                     |v||              
        | |                 ||||                     |}||              
        |||                 |{}|                     ||||             
-       |||                 ||v|                     |v||              ''', 'red');cprint('''
-      VVVVV               VvvVVVV                 VvvVVVV             
-^^^^^^^^^^^^^^^^^^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ ^ ^^^^ ^^^ ^^^^
+       |||                 ||v|                     |v||              
+      ^^^^^               ^^^^^^^                 ^^^^^^^''', 'yellow');cprint('''^^^^^^^^^^^^^^^^^^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ ^ ^^^^ ^^^ ^^^^
  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   ^^^^^^^^^^  ^^^^^^^^^^^  ^^^^ ^^
 ^^ ^^^^^^^^^^^^^^^^^^^ ^^^^ ^ ^ ^^ ^^^^^^^^^ ^^^^^^^^^^^^^^^^^  ^^^^ ^
 ^^^^^^^^^^ ^^^ ^^^^^^^ ^^^^^^^^^^ ^^^^ ^^^^^^^^^ ^^^^^^^^  ^^^^  ^^^^ 
 ^^^^ ^^ ^^^^^^ ^^^^^^^^^^^^ ^^ ^ ^^^^^ ^^^^^^^^^^^^^ ^^^ ^^ ^^  ^^^ ^^ 
-^^ ^^^^^^^^^^^^^^^^^^^ ^^^^ ^ ^ ^^ ^^^^^^^^^ ^^^^^^^^^^^^^^^^^  ^^^^ ^
-1                                                                    70
-	''','yellow')
+^^ ^^^^^^^^^^^^^^^^^^^ ^^^^ ^ ^ ^^ ^^^^^^^^^ ^^^^^^^^^^^^^^^^^  ^^^^ ^''', 'green')
+	cprint('''1^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^70''', 'yellow')
 	rand_x = random.choice([random.randrange(1, 69)])
 	for y in range(2, 20):
 		if y == 7 or y == 14:
-			guess_x = int(input("Guess where the apple is going to fall (1-69) ?"))
+			print_char(1, 17+int(y/7)+(int(y/7)+1), "Guess where the apple is going to fall (1-69) ? ", "white")
+			guess_x = int(input())
 			if guess_x == rand_x:
-				subprocess.call("clear", shell=True)
-				print("You are correct")
 				got_correct = True
 				break
-		print_char(rand_x, y, "0", "green")
-		time.sleep(0.5)
+		print_char(rand_x, y, "{}", "red")
+		time.sleep(0.3)
 		#print(rand_x)
-		print_char(rand_x+1, y, "\b ", "green")
-	print("Correct location:", rand_x)
-	time.sleep(3)
+		print_char(rand_x+1, y, " \b\b  ", "red")
+	print_char(1, 25, "Correct location: " + str(rand_x), "white")
+	print(guess_x, rand_x)
 	if got_correct:
+		print_char(1, 24, "You are correct", "white")
+		print_char(rand_x, 19, "{}", "red")
+		time.sleep(3)
+		continue
+	else:
+		print_char(rand_x, 19, "~~", "red")
+		time.sleep(3)
 		continue
