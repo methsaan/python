a938ef7bf1ad3a033dbddc7b33be958f70825577
Methsaan Wick@kali <methsaan@gmail.com>
2019-01-30 06:25:50 -0500

    print random ascii character

--- usefulprograms/asciichar.py
+++ usefulprograms/asciichar.py
@@ -2,16 +2,23 @@
 
 import subprocess as sp
 import random
+from termcolor import cprint
+import sys
 
 asciichar = []
 hexDigits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f']
 
+y = 0
 while True:
     try:
-        for x in range(1000):
+        i = y
+        for x in range(10):
             asciichar.append(eval("\"" + "\\" + "u" + random.choice(hexDigits) + random.choice(hexDigits)+ random.choice(hexDigits) + random.choice(hexDigits) + "\""))
-        for x in asciichar:
-            print(x, end=" ", flush=True)
+            y = y + 1
+        while i <= len(asciichar)-1:
+            print(asciichar[i], end=" ", flush=True)
+            i = i + 1
+        if y > 1000:
+            break
     except UnicodeEncodeError:
-        asciichar.append(eval("\"" + "\\" + "u" + random.choice(hexDigits) + random.choice(hexDigits)+ random.choice(hexDigits) + random.choice(hexDigits) + "\""))
         continue
