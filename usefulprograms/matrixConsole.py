e3c9d917e451299b5464f171dee63bf4bb31fae8
Methsaan <methsaan@gmail.com>
2019-02-11 06:49:08 -0500

    initializing matrices

--- usefulprograms/matrixConsole.py
+++ usefulprograms/matrixConsole.py
@@ -1,5 +1,19 @@
 #! /usr/bin/python2
 
-width = int(input("Enter width of matrix 1: "))
-width = int(input("Enter width of matrix 2: "))
+width1 = int(input("Enter width of matrix 1: "))
+height1 = int(input("Enter width of matrix 1: "))
+operator = input("Enter operator: ")
+matrix1 = []
+for x in range(height1):
+    matrix1.append([])
+if operator == "x":
+    height2 = width1
+    width2 = height1
+else:
+    height2 = height1
+    width2 = height2
+matrix2 = []
+for x in range(height2):
+    matrix2.append([])
+for x in range(len(matrix1)):
 
