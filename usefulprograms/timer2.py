0fe5092ef8cd46244d00cfc40c2009f62cd97d36
Methsaan <methsaan@gmail.com>
2019-02-25 13:35:05 -0500

    ;

--- usefulprograms/timer2.py
+++ usefulprograms/timer2.py
@@ -15,7 +15,7 @@ for x in range(secs):
     t.color("aqua")
     t.width(100)
     tick = 360 / secs
-    time.sleep(0.95)
+    time.sleep(.95)
     t.circle(200, tick)
 t.color("orangered")
 t.begin_fill()
