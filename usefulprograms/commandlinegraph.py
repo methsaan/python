71d507379a90390f92ca148c018a591622faba82
Methsaan <methsaan@gmail.com>
2019-07-01 09:51:51 -0400

    command line function graph

---
+++ usefulprograms/commandlinegraph.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+import time
+import datetime
+import math
+import subprocess as sp
+
+def print_char(x, y, char):
+    print("\033["+str(y)+";"+str(x)+"H"+char)
+
+function = input("Enter function to graph: ")
+sp.call("clear", shell=True)
+print_char(0, 20, "________________________________________")
+for i in range(1, 40):
+    print_char(20, i, "|")
+for x in range(-20, 20):
+    exec(function)
+    if y in range(-20, 20):
+       print_char(x+20, -y+20, "#")
+print_char(1, 1, "")
