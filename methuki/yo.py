a42ff4a1f5f6bd54c006319bbf9e28b39978e8f6
Methsaan <methsaan@gmail.com>
2019-07-16 16:45:45 -0400

    loop through array

---
+++ methuki/yo.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+name = input("what is your name? ")
+if name == "methsaan":
+    print("dear methsaan, stop farting and being an idiotic poo face")
+elif name == "methuki":
+    print("you are nice and amazing!!")
+elif name == "pamini":
+    print("you are nice")
+elif name == "mohan":
+    print("you are OK")
+else:
+    print("go away")
