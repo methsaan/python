14b00e5e69143e93c8507c1c81685941a6facdcf
Methsaan <methsaan@gmail.com>
2019-07-16 16:45:45 -0400

    loop through array

--- methuki/types.py
+++ methuki/types.py
@@ -1,4 +1,18 @@
 #! /usr/bin/python3
 
-age = float(input("how old are you? "))
-#age == 10.5
+# 10 - you are an idiot poo poo monkey face loser dude
+# 20 - meh yer OK how ya doin
+# 30 - YOU'RE AWESOME DUDE
+# other - who are you GO AWAY!!
+age = int(input("how old are you? "))
+boolean1 = age == 10
+boolean2 = age == 20
+boolean3 = age == 30
+if boolean1:
+    print("you are an idiot poo poo monkey face loser dude")
+elif boolean2:
+    print("meh yer OK how ya doin")
+elif boolean3:
+    print('YOU\'RE AWESOME DUDE')
+else:
+    print("who are you, GO AWAY!!")
