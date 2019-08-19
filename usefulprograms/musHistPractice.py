40181f8bc7be3d1f95097898d131ea65ddb9a758
Methsaan <methsaan@gmail.com>
2019-08-19 10:37:26 -0400

    music history practice

--- usefulprograms/musHistPractice.py
+++ usefulprograms/musHistPractice.py
@@ -12,4 +12,5 @@ class song:
         self.form = form
         self.tempo = tempo
         self.structure = structure
-song1 = song("A Midsummer Night's Dream"
+song1 = song("A Midsummer Night's Dream", "concert overture", "symphony orchestra", "E major", "sonata form", "unknown", "unknown")
+song2 = song("Revolutionary Etude", "solo piano work", "piano", "C minor", "ABA", "allegro con fuoco", "unknown")
