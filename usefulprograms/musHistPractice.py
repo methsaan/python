458a5b9467b32911dd824f162761fc3ef3a5fd8e
Methsaan <methsaan@gmail.com>
2019-08-22 16:11:39 -0400

    day tracker

--- usefulprograms/musHistPractice.py
+++ usefulprograms/musHistPractice.py
@@ -14,3 +14,5 @@ class song:
         self.structure = structure
 song1 = song("A Midsummer Night's Dream", "concert overture", "symphony orchestra", "E major", "sonata form", "unknown", "unknown")
 song2 = song("Revolutionary Etude", "solo piano work", "piano", "C minor", "ABA", "allegro con fuoco", "unknown")
+song3 = song("Petrushka", "ballet", "large orchestra with expanded percussion (including piano)", "unknown", "rondo form (ABACABA)", "unknown", "unknown")
+song4 = song("Dripsody", "electronic music", "recorded sound of dripping water", "unknown", "unknown", 
