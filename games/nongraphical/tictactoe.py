201cad280cc1556c9f3e0bad83d506178870312f
Methsaan <methsaan@gmail.com>
2019-08-29 14:01:24 -0400

    day tracker

--- games/nongraphical/tictactoe.py
+++ games/nongraphical/tictactoe.py
@@ -6,4 +6,13 @@ def isLinear(*nums):
     for x in range(len(nums)-1):
         difference = abs(nums[x]-nums[x+1])
         differenceList.append(difference)
+    for x in range(1, len(differenceList)):
+        if differenceList[x] != differenceList[x-1]:
+            return False
+    return True
 
+grid = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+a = (2, 4, 2)
+print(a)
+a = (4, 2, 4)
+print(a)
