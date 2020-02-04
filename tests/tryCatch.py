947b4e6280254550a7b602a904aae6e654233edf
Methsaan <methsaan@gmail.com>
2020-02-04 09:51:32 -0500

    try catch error handling

---
+++ tests/tryCatch.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+try:
+    x = input("Enter equation: ")
+    y = int(input("Enter number: "))
+    print("Answer: " + str(eval(x)/y))
+except ZeroDivisionError:
+    print("Answer: Infinity")
+except (ValueError, NameError, SyntaxError):
+    print("One of your numbers is not valid.") 
+finally:
+    print("His finish")
