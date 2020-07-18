f6d4858cf65e6348ff178fda0429253aa3487b23
Methsaan <methsaan@gmail.com>
2020-07-17 22:25:57 -0400

    less boxes

--- games/graphical/bounce.py
+++ games/graphical/bounce.py
@@ -13,7 +13,7 @@ canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
 canvas.pack()
 colors = [random.choice(["blue", "black", "orangered"]), random.choice(["springgreen", "gold", "tomato"]), random.choice(["brown", "darkgreen", "purple"])]
 canvas.create_rectangle(0, 0, WIDTH, HEIGHT, fill=colors[0], outline=colors[0])
-scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-50, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
+scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-60, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
 paddle = canvas.create_rectangle(400, HEIGHT-60, 500, HEIGHT-40, fill=colors[1], outline=colors[1])
 ball = canvas.create_oval((WIDTH/2)-15, (HEIGHT/2)-15, (WIDTH/2)+15, (WIDTH/2)+15, fill=colors[2], outline=colors[2])
 x = random.randrange(-10, 10)
@@ -38,11 +38,11 @@ while running:
     if pos[3] >= WIDTH-65:
         y = random.randrange(-8, -2)
     if pos[3] >= WIDTH-65 and (pos[2] < right_of_paddle and pos[2] > left_of_paddle):
-        scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-50, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
+        scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-60, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
         score = score + 1
         canvas.create_text(WIDTH/2, HEIGHT/2-40, text="score: "+str(score), font=("helvetica", 20), fill=colors[2])
     if pos[3] >= WIDTH-65 and (pos[2] > right_of_paddle or pos[2] < left_of_paddle):
-        scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-50, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
+        scoreboard = canvas.create_rectangle(WIDTH/2-70, HEIGHT/2-60, WIDTH/2+70, HEIGHT/2-20, fill=colors[1], outline=colors[1])
         strike = strike + 1
         canvas.create_text(WIDTH/2, HEIGHT/2-40, text="strikes: "+str(strike), font=("helvetica", 20), fill=colors[2])
         if strike == 30:
--- games/graphical/tictactoe.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-from tkinter import
-tk = Tk()
-canvas = Canvas(tk, width=600, height=600)
-canvas.pack()
-canavs.mainloop()
