f677e0525ad8d991e5a046a0f3d04b710147c6f1
Methsaan <methsaan@gmail.com>
2020-07-17 22:11:13 -0400

    delete

--- graphics/tkinter/solarSystem/sun.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-
-from tkinter import *
-tk = Tk()
-canvas = Canvas(tk, width=1500, height=700)
-canvas.pack()
-def create_sun():
-    canvas.create_oval(681, 281, 820, 420, fill="yellow")
-canvas.mainloop()
--- graphics/tkinter/solarsystem.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-
-from tkinter import *
-tk = Tk()
-canvas = Canvas(tk, width=1900, height=950)
-canvas.pack()
-canvas.mainloop()
