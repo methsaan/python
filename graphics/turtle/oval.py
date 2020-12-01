f58f280395320c6132598f3671d32805d75fc19d
Methsaan <methsaan@gmail.com>
2020-12-01 08:04:38 -0500

    list

--- graphics/turtle/oval.py
+++ graphics/turtle/oval.py
@@ -10,6 +10,22 @@ semiminor = int(input("Enter semi-minor axis: "))
 d1 = semimajor*2
 d2 = semiminor*2
 
+x = 0
+y = 0
+cntx = 0
+cnty = 0
 
+listmaj = [50, 40, 30, 20, 10]
+listmin = [50, 40, 30, 20, 10]
+
+while x < semiminor and y < semimajor:
+	t.forward(listmin[x])
+	t.left(90)
+	t.forward(listmaj[y])
+	t.right(90)
+	x += 1
+	y += 1
+	cntx += listmin[x]
+	cnty += listmaj[y]
 
 time.sleep(3)
