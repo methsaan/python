f065fcee3acf1cd86c622b38ac1826784057de3b
Methsaan <methsaan@gmail.com>
2021-03-12 22:18:29 -0500

    terms

--- usefulprograms/clock2.py
+++ usefulprograms/clock2.py
@@ -9,13 +9,21 @@ tk = Tk()
 canvas = Canvas(tk, width=800, height=800, bg="tan3")
 canvas.pack()
 
+def rotatePoints(coordsList, rotationPointCoords, angle):
+    rotatedCoords = []
+    xRotationPoint = rotationPointCoords[0]
+    yRotationPoint = rotationPointCoords[1]
+    for x in range(len(coordsList)/2):
+        
+
 canvas.create_oval(100, 100, 700, 700, fill="gray65", outline="black", width=25)
 numCoords = [["XII", 400, 150], ["I", 500, 200], ["II", 600, 300], ["III", 650, 400], ["IV", 600, 500], ["V", 500, 600], ["VI", 400, 650], ["VII", 300, 600], ["VIII", 200, 500], ["IX", 150, 400], ["X", 200, 300], ["XI", 300, 200]]
 for x in numCoords:
     canvas.create_text(x[1], x[2], text=x[0], font=("times", 30))
 
-minuteHand = canvas.create_line(400, 400, 400, 180, width=5)
-secondHand = canvas.create_line(400, 400, 400, 180, width=2, fill="red")
+hourHand = canvas.create_polygon(390, 300, 410, 300, 410, 400, 390, 400)
+minuteHand = canvas.create_polygon(395, 180, 405, 180, 405, 400, 395, 400)
+secondHand = canvas.create_polygon(399, 180, 401, 180, 401, 400, 399, 400, fill="red")
 
 tk.update()
 canvas.mainloop()
--- usefulprograms/tempTerms.py
+++ usefulprograms/tempTerms.py
@@ -2,7 +2,7 @@
 
 import random
 
-x = ["allargando", "allarg.", "arco", "attacca", "bewegt", "calando", "broadening, becoming slower", "on a bowed string instrument, resume bowing after a pizzicato passage", "proceed without a break", "moving", "becoming slower and softer", "cedez", "comodo", "con sordino", "langsam", "largamente", "leger", "lentement", "l'istesso tempo", "massig", "yeild; hold the tempo back", "at a comfortable, easy tempo", "with mute", "slow, slowly", "broadly", "light, lightly", "slowly", "the same tempo", "moderate"]
+x = ["allargando", "allarg.", "arco", "attacca", "bewegt", "calando", "broadening, becoming slower", "on a bowed string instrument, resume bowing after a pizzicato passage", "proceed without a break", "moving", "becoming slower and softer", "cedez", "comodo", "con sordino", "langsam", "largamente", "leger", "lentement", "l'istesso tempo", "massig", "yeild; hold the tempo back", "at a comfortable, easy tempo", "with mute", "slow, slowly", "broadly", "light, lightly", "slowly", "the same tempo", "moderate", "mit Ausdruck", "modere", "with expression", "at a moderate tempo", "mouvement", "tempo, motion", "pizzicato", "on a bowed string instrument, pluck the string instead of bowing", "ritenuto", "riten", "suddenly slower, held back"]
 
 y = int(input("Enter number of terms to memorize: "))
 
