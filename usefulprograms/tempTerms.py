4d84be5e2b7dd50865413d93db311766e1e03bc1
Methsaan <methsaan@gmail.com>
2021-04-30 08:26:37 -0400

    rotate one point

--- usefulprograms/tempTerms.py
+++ usefulprograms/tempTerms.py
@@ -5,7 +5,7 @@ import time
 import subprocess as sp
 
 a = ["allargando", "allarg", "arco", "attacca", "bewegt", "calando", "cedez", "comodo", "con sordino", "fp", "fortepiano", "langsam", "largamente", "leger", "lentement", "l'istesso tempo", "massig", "mit ausdruck", "modere", "mouvement", "pizzicato", "ritenuto", "riten", "schnell", "sehr", "sfz", "stringendo", "vite"]
-b = ["broadening, becoming slower", "broadening becoming slower", "on a bowed string instrument, resume bowing after a pizzicato passage", "proceed without a break", "moving", "becoming slower and softer", "yield; hold the tempo back", "at a comfortable, easy tempo", "with mute", "loud, then suddenly soft", "fortepiano", "langsam", "largamente", "leger", "lentement", "l'istesso tempo", "massig", "mit ausdruck", "modere", "mouvement", "pizzicato", "ritenuto", "riten", "schnell", "sehr", "sfz", "stringendo", "vite"]
+b = ["broadening, becoming slower", "broadening becoming slower", "on a bowed string instrument, resume bowing after a pizzicato passage", "proceed without a break", "moving", "becoming slower and softer", "yield; hold the tempo back", "at a comfortable, easy tempo", "with mute", "loud then suddenly soft", "loud, then suddenly soft", "slow, slowly", "broadly", "light, lightly", "slowly", "the same tempo", "moderate, moderately", "with expression", "at a moderate tempo", "tempo, motion", "on a bowed string instrument, pluck the string instead of bowing", "suddenly slower, held back", "suddenly slower held back", "fast", "very", "sudden strong accent of a single note or chord", "pressing, becoming faster", "fast"]
 
 y = int(input("Enter number of terms to memorize: "))
 
