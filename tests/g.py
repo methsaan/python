55db1e31f0a49c4c6a50083c6e7f70e52bc12aeb
Methsaan <methsaan@gmail.com>
2022-12-02 08:10:22 -0500

    test

---
+++ tests/g.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+def isSubset(l, subset):
+    result = True
+    for x in subset:
+        if x not in l:
+            result = False
+    return result
+
+numOfSameConstraints = input()
+
+sameConstraints = []
+
+for x in range(numOfSameConstraints):
+    sameConstraints.append(input().split())
+
+numOfDiffConstraints = input()
+
+diffConstraints = []
+
+for x in range(numOfDiffConstraints):
+    diffConstraints.append(input().split())
+
+numOfTestGroups = input()
+
+testGroups = []
+
+for x in range(numOfTestGroups):
+    testGroups.append(input().split())
+
+constraintsViolated = 0
+
+for x in testGroups:
+    for y in sameConstraints:
+        if isSubset(x, y):
+            print(x, " -xxx-", y)
+        else:
+            print(x, " -vvv-", y)
