454f3478653788aedfd7518dcae4ddc72cc1ca20
Methsaan <methsaan@gmail.com>
2023-03-22 18:47:23 -0400

    determining coordinates of rotated point from terminating angle

--- tests/rotation.py
+++ tests/rotation.py
@@ -9,7 +9,6 @@ canvas = Canvas(tk, width=600, height=600)
 canvas.pack()
 
 def rotatePoint(x, y, rotatex, rotatey, angle):
-    print(x, y)
     lengthFromRotPoint = math.sqrt(abs(x-rotatex)**2 + abs(y-rotatey)**2)
     angleStart = math.asin(abs(y-rotatey) / lengthFromRotPoint) * (180 / math.pi)
     if x < rotatex and y < rotatey:
@@ -28,34 +27,40 @@ def rotatePoint(x, y, rotatex, rotatey, angle):
         angleStart = 180
     else:
         angleStart = 0
-    terminatingAngle = angleStart + angle % 360
+    print(angleStart)
+    terminatingAngle = (angleStart + angle) % 360
+    print(terminatingAngle)
     angleEnd = None
-    if angle >= 90:
+    if terminatingAngle >= 90 and terminatingAngle < 180:
         angleEnd = 180-terminatingAngle
-    elif angle >= 180:
+    elif terminatingAngle >= 180 and terminatingAngle < 270:
         angleEnd = terminatingAngle-180
-    elif angle >= 270:
+    elif terminatingAngle >= 270 and terminatingAngle < 360:
         angleEnd = 360-terminatingAngle
     else:
         angleEnd = terminatingAngle
+    print(angleEnd)
     yFromRotPoint = abs(lengthFromRotPoint * math.sin(angleEnd))
-    xFromRotPoint = abs(lengthFromRotPoint * math.cos(angleEnd))
+    xFromRotPoint = abs(lengthFromRotPoint * math.cos(angleEnd) * (180/math.pi))
+    print(xFromRotPoint, yFromRotPoint)
     newCoordx = 0
     newCoordy = 0
-    print("angle, anglestart, terminating angle", angle, angleStart, terminatingAngle)
     if terminatingAngle >= 90 and terminatingAngle < 180:
+        print("Second quadrant")
         newCoordx = rotatex + xFromRotPoint
         newCoordy = rotatey - yFromRotPoint
     elif terminatingAngle >= 180 and terminatingAngle < 270:
+        print("Third quadrant")
         newCoordx = rotatex + xFromRotPoint
         newCoordy = rotatex + yFromRotPoint
     elif terminatingAngle >= 270 and terminatingAngle < 360:
+        print("Fourth quadrant")
         newCoordx = rotatex - xFromRotPoint
         newCoordy = rotatey + yFromRotPoint
     else:
+        print("First quadrant")
         newCoordx = rotatex - xFromRotPoint
         newCoordy = rotatey - yFromRotPoint
-    print(newCoordx, newCoordy)
     return [newCoordx, newCoordy]
 
 #def rotate(obj, rotatex, rotatey, angle):
@@ -83,58 +88,73 @@ tk.update()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 10)[0], rotatePoint(200, 200, 300, 300, 10)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 20)[0], rotatePoint(200, 200, 300, 300, 20)[1], 300, 300, width=3)
 tk.update()
-time.sleep(0.5)
-canvas.create_line(rotatePoint(200, 200, 300, 300, 20)[0], rotatePoint(200, 200, 300, 300, 20)[1], 300, 300, width=3)
-tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 30)[0], rotatePoint(200, 200, 300, 300, 30)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 40)[0], rotatePoint(200, 200, 300, 300, 40)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 50)[0], rotatePoint(200, 200, 300, 300, 50)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 60)[0], rotatePoint(200, 200, 300, 300, 60)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 70)[0], rotatePoint(200, 200, 300, 300, 70)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 80)[0], rotatePoint(200, 200, 300, 300, 80)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 90)[0], rotatePoint(200, 200, 300, 300, 90)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 100)[0], rotatePoint(200, 200, 300, 300, 100)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 110)[0], rotatePoint(200, 200, 300, 300, 110)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 120)[0], rotatePoint(200, 200, 300, 300, 120)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 130)[0], rotatePoint(200, 200, 300, 300, 130)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 140)[0], rotatePoint(200, 200, 300, 300, 140)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 150)[0], rotatePoint(200, 200, 300, 300, 150)[1], 300, 300, width=3)
 tk.update()
+print()
 time.sleep(0.5)
 canvas.create_line(rotatePoint(200, 200, 300, 300, 160)[0], rotatePoint(200, 200, 300, 300, 160)[1], 300, 300, width=3)
 tk.update()
-
-rotatePoint(6, 7, 10, 10, 90)
-rotatePoint(14, 7, 10, 10, 90)
-rotatePoint(14, 13, 10, 10, 90)
-rotatePoint(6, 13, 10, 10, 90)
+print()
+time.sleep(0.5)
+canvas.create_line(rotatePoint(200, 200, 300, 300, 170)[0], rotatePoint(200, 200, 300, 300, 170)[1], 300, 300, width=3)
+tk.update()
+print()
+time.sleep(0.5)
+canvas.create_line(rotatePoint(300, 400, 300, 300, 150)[0], rotatePoint(300, 400, 300, 300, 150)[1], 300, 300, width=3)
+tk.update()
 
 canvas.mainloop()
