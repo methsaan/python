9d387149d5917ceef1e94061c10f0cc83f9455c6
Methsaan <methsaan@gmail.com>
2023-03-25 13:41:32 -0400

    clock2.py - reuse rotate function to rotate minute, hour and second hand

--- usefulprograms/clock2.py
+++ usefulprograms/clock2.py
@@ -1,49 +1,85 @@
 #! /usr/bin/python3
 
-import datetime
-import time
 import math
-import decimal
+import time
+import datetime
 from tkinter import *
+from itertools import chain
+
 tk = Tk()
-canvas = Canvas(tk, width=800, height=800, bg="tan3")
+canvas = Canvas(tk, width=800, height=800)
 canvas.pack()
 
-def rotatePoint(x, y, xRot, yRot, angle):
-    angle2 = (angle)*(math.pi/180)
-    x1 = math.cos(angle2) * (x - xRot) - math.sin(angle2) * (y - yRot) + xRot
-    y1 = math.sin(angle2) * (x - xRot) - math.cos(angle2) * (y - yRot) + yRot
+def rotatePoint(x, y, rotatex, rotatey, angle):
+    lengthFromRotPoint = math.sqrt(abs(x-rotatex)**2 + abs(y-rotatey)**2)
+    if lengthFromRotPoint == 0:
+        return [x, y]
+    angleStart = math.asin(abs(y-rotatey) / lengthFromRotPoint) * (180 / math.pi)
+    if x < rotatex and y < rotatey:
+        pass
+    elif x > rotatex and y < rotatey:
+        angleStart = 180-angleStart
+    elif x > rotatex and y > rotatey:
+        angleStart = 180+angleStart
+    elif x < rotatex and y > rotatey:
+        angleStart = 360-angleStart
+    elif x == rotatex and y > rotatey:
+        angleStart = 270
+    elif x == rotatex and y < rotatey:
+        angleStart = 90
+    elif x > rotatex and y == rotatey:
+        angleStart = 180
+    else:
+        angleStart = 0
+    terminatingAngle = (angleStart + angle) % 360
+    angleEnd = None
+    if terminatingAngle >= 90 and terminatingAngle < 180:
+        angleEnd = 180-terminatingAngle
+    elif terminatingAngle >= 180 and terminatingAngle < 270:
+        angleEnd = terminatingAngle-180
+    elif terminatingAngle >= 270 and terminatingAngle < 360:
+        angleEnd = 360-terminatingAngle
+    else:
+        angleEnd = terminatingAngle
+    yFromRotPoint = abs(lengthFromRotPoint * math.sin(math.radians(angleEnd)))
+    xFromRotPoint = abs(lengthFromRotPoint * math.cos(math.radians(angleEnd)))
+    newCoordx = 0
+    newCoordy = 0
+    if terminatingAngle >= 90 and terminatingAngle < 180:
+        newCoordx = rotatex + xFromRotPoint
+        newCoordy = rotatey - yFromRotPoint
+    elif terminatingAngle >= 180 and terminatingAngle < 270:
+        newCoordx = rotatex + xFromRotPoint
+        newCoordy = rotatex + yFromRotPoint
+    elif terminatingAngle >= 270 and terminatingAngle < 360:
+        newCoordx = rotatex - xFromRotPoint
+        newCoordy = rotatey + yFromRotPoint
+    else:
+        newCoordx = rotatex - xFromRotPoint
+        newCoordy = rotatey - yFromRotPoint
+    return [newCoordx, newCoordy]
 
-    #x1 = math.cos(angle * (math.pi / 180)) * (x - xRot) - math.sin(angle * (math.pi / 180)) * (y - yRot) + xRot
-    #y1 = math.sin(angle * (math.pi / 180)) * (x - xRot) - math.cos(angle * (math.pi / 180)) * (y - yRot) + yRot
-    return [x1, y1]
+def rotate(obj, rotatex, rotatey, angle):
+    oldCoords = []
+    for x in range(0, len(canvas.coords(obj)), 2):
+        oldCoords.append([canvas.coords(obj)[x], canvas.coords(obj)[x+1]])
+    newCoords = []
+    for x in oldCoords:
+        newCoords.append([rotatePoint(x[0], x[1], rotatex, rotatey, angle)[0], rotatePoint(x[0], x[1], rotatex, rotatey, angle)[1]])
+    canvas.coords(obj, *list(chain.from_iterable(newCoords)))
 
-def rotatePoints(coordsList, rotationPointCoords, angle):
-    rotatedCoords = []
-    for x in range(0, len(coordsList), 2):
-        rotatedCoords.append(rotatePoint(coordsList[x], coordsList[x+1], rotationPointCoords[0], rotationPointCoords[1], angle)[0])
-        rotatedCoords.append(rotatePoint(coordsList[x], coordsList[x+1], rotationPointCoords[0], rotationPointCoords[1], angle)[1])
+hourHand = canvas.create_polygon(390, 240, 410, 240, 410, 410, 390, 410, fill="black")
+minuteHand = canvas.create_polygon(395, 130, 405, 130, 405, 405, 395, 405, fill="black")
+secondHand = canvas.create_polygon(399, 130, 401, 130, 401, 401, 399, 401, fill="red")
 
-    return rotatedCoords
-
-canvas.create_oval(100, 100, 700, 700, fill="gray65", outline="black", width=25)
-numCoords = [["XII", 400, 150], ["I", 500, 200], ["II", 600, 300], ["III", 650, 400], ["IV", 600, 500], ["V", 500, 600], ["VI", 400, 650], ["VII", 300, 600], ["VIII", 200, 500], ["IX", 150, 400], ["X", 200, 300], ["XI", 300, 200]]
-for x in numCoords:
-    canvas.create_text(x[1], x[2], text=x[0], font=("times", 30))
-
-hourHand = canvas.create_polygon(390, 300, 410, 300, 410, 400, 390, 400)
-minuteHand = canvas.create_polygon(395, 180, 405, 180, 405, 400, 395, 400)
-secondHand = canvas.create_polygon(399, 180, 401, 180, 401, 400, 399, 400, fill="red")
-
-coordx = 200
-coordy = 200
-
-for x in range(0, 360, 4):
-    rotPoints = rotatePoint(200, 200, 400, 400, x)
-    canvas.create_oval(rotPoints[0]-10, rotPoints[1]-10, rotPoints[0]+10, rotPoints[1]+10, fill="green")
-    time.sleep(1)
+while True:
+    canvas.coords(hourHand, 390, 240, 410, 240, 410, 410, 390, 410)
+    canvas.coords(minuteHand, 395, 130, 405, 130, 405, 405, 395, 405)
+    canvas.coords(secondHand, 399, 130, 401, 130, 401, 401, 399, 401)
+    rotate(minuteHand, 400, 400, datetime.datetime.now().minute*6)
+    rotate(secondHand, 400, 400, datetime.datetime.now().second*6)
+    #rotate(hourHand, 400, 400, 3)
     tk.update()
-
-# rewrite rotatepoint function
+    #time.sleep(0.03)
 
 canvas.mainloop()
