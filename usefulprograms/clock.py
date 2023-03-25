216425d016f0d803e07bd5e376540f337e041fc0
Methsaan <methsaan@gmail.com>
2023-03-25 14:53:02 -0400

    clock.py - draw a line using text, given beginning and end coordinate on terminal

--- usefulprograms/clock.py
+++ usefulprograms/clock.py
@@ -10,7 +10,7 @@ from sys import *
 def print_char(x, y, char):
     print("\033["+str(y)+";"+str(x)+"H"+char)
 
-def print_line(x1, y1, x2, y2):
+def print_line(x1, y1, x2, y2, c):
     if y2-y1 != 0:
         xInc = 1 if (y2-y1) < (x2-x1) else ((x2-x1)/(y2-y1) if (y2-y1) != 0 else 1)
         yInc = 1 if (x2-x1) < (y2-y1) else ((y2-y1)/(x2-x1) if (x2-x1) != 0 else 1)
@@ -21,6 +21,6 @@ def print_line(x1, y1, x2, y2):
     numOfCoords = x2-x1 if (xInc == 1 or y2-y1 == 0) else y2-y1
     for x in range(abs(numOfCoords)):
         temp = -x if numOfCoords < 0 else x
-        print_char(x1+int(temp*xInc), y1+int(temp*yInc), "#")
+        print_char(x1+int(temp*xInc), y1+int(temp*yInc), c)
 
-sp.call("clear", shell=True)
+print_line(20, 10, 30, 20, "\\")
