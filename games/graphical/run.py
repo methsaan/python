54bd00fa9c18845821486ca93649bf1e24491ce4
Methsaan Wickramasinghe <methsaan@gmail.com>
2024-03-24 12:36:51 -0400

    run.py - run on road, dodge and jump over cars

--- games/graphical/run.py
+++ games/graphical/run.py
@@ -1,16 +1,139 @@
 #! /usr/bin/python3
 
+WIDTH = 900
+HEIGHT = 800
+
+class RoadObj:
+    def __init__(self, edges, xPosRef, yPosRef):
+        # xPos - horizontal reference point 0 = start of left lane, 1 = end of right lane
+        self.xPosRef = xPosRef
+        # yPos - vertical reference point 0 = top of road, 1 = bottom of road
+        self.yPosRef = yPosRef
+        # edges - list of vector tuples of edges (x1, y1, x2, y2) - all 0-1
+        self.edges = edges
+        self.canvasCoords = []
+    def getCanvasCoords(self):
+        self.canvasCoords = []
+        for edge in self.edges:
+            leftLaneStart = WIDTH*(8 - 3*(self.yPosRef + edge[1]))/20  # 2*WIDTH/5 - (3*WIDTH/20)*(self.yPosRef + edge[1])
+            roadWidth = WIDTH*(2 + 3*(self.yPosRef + edge[1]))/10 # WIDTH/5 + (3*WIDTH/10)*(self.yPosRef + edge[1])
+            leftLaneStart2 = WIDTH*(8 - 3*(self.yPosRef + edge[3]))/20
+            roadWidth2 = WIDTH*(2 + 3*(self.yPosRef + edge[3]))/10
+            xCoord1 = leftLaneStart + roadWidth * self.xPosRef + roadWidth * edge[0]
+            xCoord2 = leftLaneStart2 + roadWidth2 * self.xPosRef + roadWidth2 * edge[2]
+            yCoord1 = HEIGHT * self.yPosRef + HEIGHT * edge[1]
+            yCoord2 = HEIGHT * self.yPosRef + HEIGHT * edge[3]
+            self.canvasCoords.append((xCoord1, yCoord1, xCoord2, yCoord2))
+        return self.canvasCoords
+
+import time
+import random
+import math
 from tkinter import *
 
-WIDTH = 800
-HEIGHT = 600
-
 tk = Tk()
-canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
+canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="OliveDrab4")
 canvas.pack()
 
-class Player:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.body = []
+# road
+canvas.create_polygon(2*WIDTH/5, 0, 3*WIDTH/5, 0, 3*WIDTH/4, HEIGHT, WIDTH/4, HEIGHT, fill="gray20", outline="gray20")
+
+boxEdges = [(0, 0, (1/3), 0), ((1/3), 0, (1/3), (1/6)), ((1/3), (1/6), 0, (1/6)), (0, (1/6), 0, 0)]
+#boxEdges = [((1/6), (1/24), (2/9), (1/18)), ((2/9), (1/18), (1/4), (1/12)), ((1/4), (1/12), (2/9), (1/9)),\
+#            ((2/9), (1/9), (1/6), (1/8)), ((1/6), (1/8), (1/9), (1/9)), ((1/9), (1/9), (1/12), (1/12)),\
+#            ((1/12), (1/12), (1/9), (1/18)), ((1/9), (1/18), (1/6), (1/24))]
+
+boxes = []
+boxSprites = []
+
+colors = ["sienna4", "DarkOrange4", "coral4", "tan2", "gold4", "honeydew4", "saddle brown", "SkyBlue4", "gray50", "tomato4"] * 17
+random.shuffle(colors)
+
+for x in range(54):
+    laneStarts = [0, (1/3), (2/3)]
+    for y in range(3):
+        box = RoadObj(boxEdges, laneStarts[y], -54+x+random.randrange(90)/100)
+        boxes.append(box)
+        c = box.getCanvasCoords()
+        boxSprites.append(canvas.create_polygon(c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1], c[3][0], c[3][1], fill=colors[x*3+y]))
+        #boxSprites.append(canvas.create_polygon(c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1], c[3][0], c[3][1], c[4][0], c[4][1], c[5][0], c[5][1], c[6][0], c[6][1], c[7][0], c[7][1],  fill=colors[x*3+y]))
+
+scoreBoard = canvas.create_text(WIDTH/2, HEIGHT/10, text="Score: 0", font=("helvetica", 20), fill="navy")
+#human = canvas.create_rectangle(WIDTH*0.485, HEIGHT*0.6, WIDTH*0.515, HEIGHT*0.95, fill="black")
+human = []
+human.append(canvas.create_line(WIDTH*0.475, HEIGHT*0.95, WIDTH*0.5, HEIGHT*0.90, fill="black", width=4))
+human.append(canvas.create_line(WIDTH*0.525, HEIGHT*0.95, WIDTH*0.5, HEIGHT*0.90, fill="black", width=4))
+human.append(canvas.create_line(WIDTH*0.5, HEIGHT*0.90, WIDTH*0.5, HEIGHT*0.80, fill="black", width=4))
+human.append(canvas.create_line(WIDTH*0.475, HEIGHT*0.85, WIDTH*0.5, HEIGHT*0.80, fill="black", width=4))
+human.append(canvas.create_line(WIDTH*0.525, HEIGHT*0.85, WIDTH*0.5, HEIGHT*0.80, fill="black", width=4))
+human.append(canvas.create_rectangle(WIDTH*0.49, HEIGHT*0.76, WIDTH*0.51, HEIGHT*0.80, fill="black"))
+humanLane = 2
+humanJumpHeight = 0
+humanJumpIncrement = -HEIGHT*0.08
+jump = False
+lost = False
+
+def move_paddle(event):
+    global humanLane
+    global jump
+    if event.keysym == 'Left':
+        humanLane -= 1
+        if humanLane < 1:
+            humanLane = 1
+    elif event.keysym == 'Right':
+        humanLane += 1
+        if humanLane > 3:
+            humanLane = 3
+    elif event.keysym == 'Up':
+        jump = True
+canvas.bind_all('<KeyPress-Left>', move_paddle)
+canvas.bind_all('<KeyPress-Right>', move_paddle)
+canvas.bind_all('<KeyPress-Up>', move_paddle)
+
+boxesPassedCnt = 0
+boxesPassed = []
+score = 0
+for x in range(30*54):
+    for y in range(162):
+        boxes[y].yPosRef += (1/30)
+        c = boxes[y].getCanvasCoords()
+        canvas.coords(boxSprites[y], c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1], c[3][0], c[3][1])
+        #canvas.coords(boxSprites[y], c[0][0], c[0][1], c[1][0], c[1][1], c[2][0], c[2][1], c[3][0], c[3][1], c[4][0], c[4][1], c[5][0], c[5][1], c[6][0], c[6][1], c[7][0], c[7][1])
+        if c[2][1] > 0 and boxes[y] not in boxesPassed:
+            boxesPassedCnt += 1
+            boxesPassed.append(boxes[y])
+    canvas.coords(human[0], WIDTH*0.475+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[0])[1], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[0])[3])
+    canvas.coords(human[1], WIDTH*0.525+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[1])[1], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[1])[3])
+    canvas.coords(human[2], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[2])[1], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[2])[3])
+    canvas.coords(human[3], WIDTH*0.475+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[3])[1], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[3])[3])
+    canvas.coords(human[4], WIDTH*0.525+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[4])[1], WIDTH*0.5+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[4])[3])
+    canvas.coords(human[5], WIDTH*0.49+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[5])[1], WIDTH*0.51+((humanLane-2)*WIDTH*(8 - 3*0.95)/20)/2, canvas.coords(human[5])[3])
+    if jump:
+        for i in human:
+            canvas.move(i, 0, humanJumpIncrement)
+        humanJumpHeight -= humanJumpIncrement
+        if humanJumpHeight == HEIGHT*0.72:
+            humanJumpIncrement = -humanJumpIncrement
+        if humanJumpHeight == 0 and humanJumpIncrement > 0:
+            humanJumpIncrement = -humanJumpIncrement
+            jump = False
+    else:
+        visibleBoxes = boxesPassed[boxesPassedCnt-6:]
+        for i in range(len(visibleBoxes)):
+            c = visibleBoxes[i].getCanvasCoords()
+            humanPos = canvas.coords(human[0])[2]
+            if c[0][0] < humanPos and humanPos < c[1][0] and c[0][1] < HEIGHT*0.95 and HEIGHT*0.95 < c[2][1]:
+                lost = True
+    score = math.floor(x / (30*54-1) * 10000)
+    canvas.itemconfig(scoreBoard, text=("Score: " + str(score)))
+    tk.update()
+    time.sleep(0.01)
+    if lost:
+        break
+
+if lost:
+    canvas.create_text(WIDTH/2, HEIGHT/2, text=("You lost. Score: " + str(score)), font=("helvetica", 50), fill="navy")
+else:
+    canvas.create_text(WIDTH/2, HEIGHT/2, text=("You won. Score: " + str(score)), font=("helvetica", 50), fill="navy")
+
+canvas.mainloop()
