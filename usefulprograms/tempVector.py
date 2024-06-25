e84ea9139a715ae44b7a3294754973a210f44aa4
Methsaan Wickramasinghe <methsaan@gmail.com>
2024-06-25 18:53:35 -0400

    tempVector.py - place trees on field, rotate test

--- usefulprograms/tempVector.py
+++ usefulprograms/tempVector.py
@@ -5,7 +5,7 @@ import random
 import math
 import time
 
-WIDTH = 300
+WIDTH = 900
 HEIGHT = 900
 
 # *Redefine
@@ -34,10 +34,12 @@ class Point3D:
         return xCoord, yCoord
 
 class Shape3D:
-    def __init__(self, *points3d):
+    def __init__(self, *points3d, color, outlineWidth):
         # Set initial coordinates of shape vertices
         self.points = list(points3d)
         self.canvasObj = None
+        self.color = color
+        self.outlineWidth = outlineWidth
     def rotate(self, x, y, z):
         # Transform all points given transformation matrix
         for point in self.points:
@@ -47,9 +49,26 @@ class Shape3D:
         for point in self.points:
             coordsList.append(list(point.get2DCoords()))
         if self.canvasObj == None:
-            self.canvasObj = c.create_polygon(*sum(coordsList, []), fill="green")
+            self.canvasObj = c.create_polygon(*sum(coordsList, []), fill=self.color, outline="black", width=self.outlineWidth)
         else:
             c.coords(self.canvasObj, *sum(coordsList, []))
+
+class Tree:
+    def __init__(self, posx3d, posy3d, width, height, c):
+        self.base = [Point3D(posx3d+width/2, posy3d+width/2, 0), Point3D(posx3d+width/2, posy3d-width/2, 0), Point3D(posx3d-width/2, posy3d-width/2, 0), Point3D(posx3d-width/2, posy3d+width/2, 0)]
+        self.side1 = [Point3D(posx3d+width/2, posy3d+width/2, 0), Point3D(posx3d+width/2, posy3d-width/2, 0), Point3D(posx3d+width/2, posy3d-width/2, height), Point3D(posx3d+width/2, posy3d+width/2, height)]
+        self.side2 = [Point3D(posx3d+width/2, posy3d-width/2, 0), Point3D(posx3d-width/2, posy3d-width/2, 0), Point3D(posx3d-width/2, posy3d-width/2, height), Point3D(posx3d+width/2, posy3d-width/2, height)]
+        self.side3 = [Point3D(posx3d-width/2, posy3d-width/2, 0), Point3D(posx3d-width/2, posy3d+width/2, 0), Point3D(posx3d-width/2, posy3d+width/2, height), Point3D(posx3d-width/2, posy3d-width/2, height)]
+        self.side4 = [Point3D(posx3d-width/2, posy3d+width/2, 0), Point3D(posx3d+width/2, posy3d+width/2, 0), Point3D(posx3d+width/2, posy3d+width/2, height), Point3D(posx3d-width/2, posy3d+width/2, height)]
+        self.top = [Point3D(posx3d+width/2, posy3d+width/2, height), Point3D(posx3d+width/2, posy3d-width/2, height), Point3D(posx3d-width/2, posy3d-width/2, height), Point3D(posx3d-width/2, posy3d+width/2, height)]
+        self.facePoints = [self.base, self.side2, self.side3, self.side1, self.side4, self.top]
+        self.faces = [Shape3D(*i, color="brown", outlineWidth=2) for i in self.facePoints]
+        for i in self.faces:
+            i.updateCoords(c)
+    def rotate(self, x, y, z, c):
+        for i in self.faces:
+            i.rotate(x, y, z)
+            i.updateCoords(c)
 
 def vectorTransform(transformMatrix, vector):
     matrixProductx = round(transformMatrix[0][0]*vector[0] + transformMatrix[0][1]*vector[1] + transformMatrix[0][2]*vector[2], 4)
@@ -95,28 +114,56 @@ canvas.create_line(WIDTH/2, 0, WIDTH/2, HEIGHT, width=2)
 canvas.create_line(0, HEIGHT/2, WIDTH, HEIGHT/2, width=2)
 
 for x in range(int(math.sqrt(WIDTH/2))):
-    canvas.create_line(WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(41/80))
-    canvas.create_line(WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(41/80))
+    if x == int(math.sqrt(WIDTH/2)) - 1:
+        canvas.create_line(WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(41/80), width=2, fill="orange")
+        canvas.create_line(WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(41/80), width=2, fill="orange")
+    else:
+        canvas.create_line(WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2-x*math.sqrt(WIDTH/2), HEIGHT*(41/80))
+        canvas.create_line(WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(39/80), WIDTH/2+x*math.sqrt(WIDTH/2), HEIGHT*(41/80))
 for x in range(int(math.sqrt(HEIGHT/2))):
-    canvas.create_line(WIDTH*(39/80), HEIGHT/2+x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2+x*math.sqrt(HEIGHT/2))
-    canvas.create_line(WIDTH*(39/80), HEIGHT/2-x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2-x*math.sqrt(HEIGHT/2))
+    if x == int(math.sqrt(HEIGHT/2)) - 1:
+        canvas.create_line(WIDTH*(39/80), HEIGHT/2+x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2+x*math.sqrt(HEIGHT/2), width=2, fill="orange")
+        canvas.create_line(WIDTH*(39/80), HEIGHT/2-x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2-x*math.sqrt(HEIGHT/2), width=2, fill="orange")
+    else:
+        canvas.create_line(WIDTH*(39/80), HEIGHT/2+x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2+x*math.sqrt(HEIGHT/2))
+        canvas.create_line(WIDTH*(39/80), HEIGHT/2-x*math.sqrt(HEIGHT/2),  WIDTH*(41/80), HEIGHT/2-x*math.sqrt(HEIGHT/2))
 for x in range(int(math.sqrt(HEIGHT))):
     canvas.create_line(WIDTH*(39/80)-x*(math.sqrt(WIDTH/2)/math.sqrt(2)), HEIGHT/2+x*(math.sqrt(HEIGHT/2)/math.sqrt(2)), WIDTH*(41/80)-x*(math.sqrt(WIDTH/2)/math.sqrt(2)), HEIGHT/2+x*(math.sqrt(HEIGHT/2)/math.sqrt(2)))
     canvas.create_line(WIDTH*(39/80)+x*(math.sqrt(WIDTH/2)/math.sqrt(2)), HEIGHT/2-x*(math.sqrt(HEIGHT/2)/math.sqrt(2)), WIDTH*(41/80)+x*(math.sqrt(WIDTH/2)/math.sqrt(2)), HEIGHT/2-x*(math.sqrt(HEIGHT/2)/math.sqrt(2)))
 
 fieldPoints = [Point3D(10, -10, 0), Point3D(10, 10, 0), Point3D(-10, 10, 0), Point3D(-10, -10, 0)]
-field = Shape3D(*fieldPoints)
+field = Shape3D(*fieldPoints, color="green", outlineWidth=2)
 field.updateCoords(canvas)
 
+trees = []
+
+for x in range(5):
+    for y in range(5):
+        trees.append(Tree(-8+x*4 + random.randrange(-19, 19)/10, -8+y*4 + random.randrange(-19, 19)/10, random.randrange(3, 10)/10, random.randrange(5, 15), canvas))
 tk.update()
-time.sleep(1)
-for j in range(33):
-    field.rotate(0, 1, 0)
-    field.updateCoords(canvas)
-    tk.update()
-    time.sleep(0.05)
-    print([p.v for p in field.points])
-tk.update()
+
+while True:
+    for j in range(36):
+        field.rotate(0, 10, 0)
+        for i in range(25):
+            trees[i].rotate(0, 10, 0, canvas)
+        field.updateCoords(canvas)
+        tk.update()
+        print([p.v for p in field.points])
+    for j in range(36):
+        field.rotate(0, 0, 10)
+        for i in range(25):
+            trees[i].rotate(0, 0, 10, canvas)
+        field.updateCoords(canvas)
+        tk.update()
+        print([p.v for p in field.points])
+    for j in range(36):
+        field.rotate(10, 0, 0)
+        for i in range(25):
+            trees[i].rotate(10, 0, 0, canvas)
+        field.updateCoords(canvas)
+        tk.update()
+        print([p.v for p in field.points])
 
 
 canvas.mainloop()
