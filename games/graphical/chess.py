0a1232f969c5a213425590dfe20b1507bcddeefd
Methsaan Wickramasinghe <methsaan@gmail.com>
2024-08-19 09:48:21 -0400

    chess.py - set up occupancy grid for self and opponent- grid showing whether space on board is occupied - testing

--- games/graphical/chess.py
+++ games/graphical/chess.py
@@ -13,6 +13,8 @@ class ChessPiece:
         self.linear = linear
         self.allMoves = None
         self.allAvailableMoves = None
+    def getPosition(self):
+        return (self.posx, self.posy)
 
 class Pawn(ChessPiece):
     def __init__(self, color, side, posx, posy):
@@ -52,26 +54,81 @@ class Bishop(ChessPiece):
 
 class OccupancyGrid:
     def __init__(self):
-        self.columns = [[0 for x in range(10)] for y in range(10)]
+        self.columns = [[0 for x in range(8)] for y in range(8)]
+    def occupySquare(self, x, y):
+        self.columns[x][y] = 1
+    def unoccupySquare(self, x, y):
+        self.columns[x][y] = 0
+    def occupyRow(self, y):
+        for i in range(8):
+            self.columns[i][y] = 1
+    def unoccupyRow(self, y):
+        for i in range(8):
+            self.columns[i][y] = 0
+    def occupyColumn(self, x):
+        self.columns[x] = [1 for i in range(8)]
+    def unoccupyColumn(self, x):
+        self.columns[x] = [0 for i in range(8)]
+    def reset(self):
+        self.columns = [[0 for x in range(8)] for y in range(8)]
 
 class Player:
-    def __init__(self, side, color):
+    def __init__(self, side, color, opponent=None):
         self.side = side
         self.color = color
         self.pawns = [Pawn(self.color, self.side, i+1, 2) for i in range(8)]
         self.castles = [Castle(self.color, self.side, 1, 1), Castle(self.color, self.side, 8, 1)]
         self.knights = [Knight(self.color, self.side, 2, 1), Knight(self.color, self.side, 7, 1)]
         self.bishops = [Bishop(self.color, self.side, 3, 1), Bishop(self.color, self.side, 6, 1)]
-        self.king = King(self.color, self.side, 4, 1) if side == "up" else King(self.color, self.side, 5, 1) 
-        self.queen = Queen(self.color, self.side, 5, 1) if side == "up" else Queen(self.color, self.side, 4, 1) 
-    def setAvailableMoves(self):
+        self.king = King(self.color, self.side, 4, 1) if side == "up" else King(self.color, self.side, 5, 1)
+        self.queen = Queen(self.color, self.side, 5, 1) if side == "up" else Queen(self.color, self.side, 4, 1)
+        self.pieces = [self.king, self.queen] + self.castles + self.knights + self.pawns + self.bishops
+        self.occupancyGridSelf = OccupancyGrid()
+        self.occupancyGridSelf.occupyRow(1)
+        self.occupancyGridSelf.occupyRow(2)
+        self.opponent = opponent
+        self.occupancyGridOpp = OccupancyGrid()
+        if opponent != None:
+            self.occupancyGridOpp = self.opponent.occupancyGridSelf
+    def updateOppOccupancy(self, opponent=None):
+        if opponent != None:
+            self.opponent = opponent
+        self.occupancyGridOpp = self.opponent.occupancyGridSelf
+    def updateSelfOccupancy(self):
+        self.occupancyGridSelf.reset()
+        for piece in self.pieces:
+            coords = piece.getPosition()
+            self.occupancyGridSelf.occupySquare(coords[0], coords[1])
+    #def setAvailableMoves(self):
         # Set allAvailableMoves for queen, bishops and castles given directions and spaces available
         # Set allAvailableMoves for king, knights and pawns given allMoves and spaces available
+        # Subtract opposing player's occupied spaces, spaces occupied by own pieces and spaces outside grid from allMoves and spaces
+        # in all directions
+
 
 from tkinter import *
 
 tk = Tk()
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
 canvas.pack()
+
+computer = Player("up", "black")
+user = Player("down", "white", computer)
+computer.updateOppOccupancy(user)
+
+user.pawns[5].posy += 2
+user.updateSelfOccupancy()
+computer.updateOppOccupancy()
+
+computer.knight[1].posx -= 1
+computer.knight[1].posy += 2
+computer.updateSelfOccupancy()
+user.updateOppOccupancy()
+
+for i in user.occupancyGridSelf:
+    print(i)
+
+for i in computer.occupancyGridSelf:
+    print(i)
 
 canvas.mainloop()
