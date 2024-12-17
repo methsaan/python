fd3ceb554dcfb31958d2f7f8e013e3b44e69a895
Methsaan Wickramasinghe <methsaan@gmail.com>
2024-12-17 18:40:59 -0500

    curveSketch.py - sin/cos of a function - differentiate outer, differentiate constant

--- usefulprograms/curveSketch.py
+++ usefulprograms/curveSketch.py
@@ -6,7 +6,11 @@ import re
 #canvas = Canvas(tk, width=1000, height=1000)
 #canvas.pack()
 
-allowedFuncRegex = "x|\*|\*\*|math\.sin|math\.cos|\d|math.log|\+| |\-|\/"
+"""
+(?<=\*\*)(?<=(.*\).*){0})(?<=(.*\(.*){0})\d+|(?<=\*\*)(?<=(.*\).*){1})(?<=(.*\(.*){1})\d+|(?<=\*\*)(?<=(.*\).*){2})(?<=(.*\(.*){2})\d+|(?<=\*\*)(?<=(.*\).*){3})(?<=(.*\(.*){3})\d+|(?<=\*\*)(?<=(.*\).*){4})(?<=(.*\(.*){4})\d+|(?<=\*\*)(?<=(.*\).*){5})(?<=(.*\(.*){5})\d+|(?<=\*\*)(?<=(.*\).*){6})(?<=(.*\(.*){6})\d+|(?<=\*\*)(?<=(.*\).*){7})(?<=(.*\(.*){7})\d+|(?<=\*\*)(?<=(.*\).*){8})(?<=(.*\(.*){8})\d+
+"""
+
+allowedFuncRegex = r"[x|\*{1,2}|math\.sin\(.+?\)|math\.cos\(.+?\)|\d|math.log\(.+?\)|\+| |\-|\/]+"
 
 class Function:
     def __init__(self, funcExp):
@@ -21,22 +25,58 @@ class Function:
         Differentiate simple term or apply chain rule only to the most outer
         function of a composite function term
         """
-        if re.search("-*math\.sin\(({allowedFuncRegex})\)", exp) != None:
-            # math.sin(x^2 + math.log(x))
+        print("Original:", exp)
+        if re.search(r"\d+\*\(" + allowedFuncRegex + r"\)\*\*\d+", exp) != None:
+            # power function of a function
+            # 22*(x**2 + math.sin(x))**5
+            print("power function of a triangle")
+            powerRegex = ""
+            for bracketCnt in range(8):
+                powerRegexTemp = r"(?<=\*\*)"
+                powerRegexTemp += r"(?<=(.*\).*){" + str(bracketCnt) + r"})"
+                powerRegexTemp += r"(?<=(.*\(.*){" + str(bracketCnt) + r"})"
+                powerRegexTemp += "\d+"
+                powerRegex += powerRegexTemp + "|"
+            powerRegexTemp = r"(?<=\*\*)"
+            powerRegexTemp += r"(?<=(.*\).*){8})"
+            powerRegexTemp += r"(?<=(.*\(.*){8})"
+            powerRegexTemp += "\d+"
+            powerRegex += powerRegexTemp
+            
+            print(powerRegex)
+            print("power:", re.findall(powerRegex, exp))
+            print("constant:", re.findall(r"(?=\*)\d+", exp))
+            print("function:", re.findall(r"\(" + allowedFuncRegex + r"\)", exp))
+            return str(int(re.findall(r"(?<=\*\*)\d+", exp)[0]) * int(re.findall(r"(?=\*)\d+", exp)[0])) + \
+                   re.findall(r"\(" + allowedFuncRegex + r"\)")[0] + str(int(re.findall(r"(?<=\*\*)\d+", exp)[0]) - 1)
+        elif re.search(r"-*math\.sin\(" + allowedFuncRegex + r"\)", exp) != None:
+            # sin of a function
+            # math.sin(x**2 + math.log(x))
+            print("sin of a triangle")
             return exp.replace("math.sin", "math.cos")
-        elif re.search("-*math\.cos\(({allowedFuncRegex})\)", exp) != None:
-            # math.cos(x^2 - 4x + 1)
+        elif re.search(r"-*math\.cos\(" + allowedFuncRegex + r"\)", exp) != None:
+            # cos of a function
+            # math.cos(x**2 - 4*x + 1)
+            print("cos of a triangle")
             return exp.replace("math.cos", "-math.sin")
         elif exp.isdigit():
+            # constant
             # 46
+            print("constant")
             return "0"
-        elif re.search("\d+\*\(({allowedFuncRegex})\)\^\d+", exp) != None:
-            # 22*(x^2 + math.sin(x))^5
-            return str(int(re.findall("(?<=\^)\d+", exp)[0]) * int(re.findall("(?=\*)\d+", exp)[0])) + \
-                   re.findall("\(({allowedFuncRegex})\)") + str(int(re.findall("(?<=\^)\d+", exp)[0]) - 1)
 
 func = Function("x**2 + 2*x + 1")
 print(func.f(5))
 print(func.yInt())
+
+string1 = "math.sin(x**2 + math.log(x))"
+string2 = "math.cos(x**2 - 4*x + 1)"
+string3 = "46"
+string4 = "22*((x+2)**2 + math.sin(x))**5"
+
+print(func.differentiateBasic(string1))
+print(func.differentiateBasic(string2))
+print(func.differentiateBasic(string3))
+print(func.differentiateBasic(string4))
 
 #canvas.mainloop()
